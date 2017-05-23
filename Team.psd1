0f7054c7957e0682d9e9c958233a41c2e083b2e0
Donovan Brown <dbrown@microsoft.com>
2017-05-22 22:49:26 -0400

    Added Get-BuildLog

--- Team.psd1
+++ Team.psd1
@@ -114,7 +114,8 @@
       'Update-Project',
       'Get-GitRepository',
       'Add-GitRepository',
-      'Remove-GitRepository')
+      'Remove-GitRepository',
+      'Get-BuildLog')
 
    # Cmdlets to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no cmdlets to export.
    # CmdletsToExport = @()
--- src/builds.psm1
+++ src/builds.psm1
@@ -6,12 +6,14 @@ $here = Split-Path -Parent $MyInvocation.MyCommand.Path
 
 function _buildURL {
    param(
-      [parameter(Mandatory=$true)]
+      [parameter(Mandatory = $true)]
       [string] $ProjectName,
-      [int] $Id
+      [int] $Id,
+      [Switch] $Logs,
+      [int] $LogIndex
    )
 
-   if(-not $env:TEAM_ACCT) {
+   if (-not $env:TEAM_ACCT) {
       throw 'You must call Add-TeamAccount before calling any other functions in this module.'
    }
 
@@ -19,8 +21,16 @@ function _buildURL {
    $resource = "/build/builds"
    $instance = $env:TEAM_ACCT
 
-   if($id) {
+   if ($id) {
       $resource += "/$id"
+   }
+
+   if ($Logs.IsPresent) {
+      $resource += "/logs"
+
+      if ($LogIndex) {
+         $resource += "/$LogIndex"
+      }
    }
 
    # Build the url to list the projects
@@ -42,53 +52,53 @@ function _applyTypes {
    $item.repository.PSObject.TypeNames.Insert(0, 'Team.Repository')
    $item.definition.PSObject.TypeNames.Insert(0, 'Team.BuildDefinition')
 
-   if($item.PSObject.Properties.Match('queue').count -gt 0 -and $item.queue -ne $null) {
+   if ($item.PSObject.Properties.Match('queue').count -gt 0 -and $item.queue -ne $null) {
       $item.queue.PSObject.TypeNames.Insert(0, 'Team.Queue')
    }
 
-   if($item.PSObject.Properties.Match('orchestrationPlan').count -gt 0 -and $item.orchestrationPlan -ne $null) {
+   if ($item.PSObject.Properties.Match('orchestrationPlan').count -gt 0 -and $item.orchestrationPlan -ne $null) {
       $item.orchestrationPlan.PSObject.TypeNames.Insert(0, 'Team.OrchestrationPlan')
    }
 }
 
 function Get-Build {
-   [CmdletBinding(DefaultParameterSetName='List')]
+   [CmdletBinding(DefaultParameterSetName = 'List')]
    param (
-      [Parameter(ParameterSetName='List')]
+      [Parameter(ParameterSetName = 'List')]
       [int] $Top,
 
-      [Parameter(ParameterSetName='List')]
+      [Parameter(ParameterSetName = 'List')]
       [ValidateSet('succeeded', 'partiallySucceeded', 'failed', 'canceled')]
       [string] $ResultFilter,
 
-      [Parameter(ParameterSetName='List')]
+      [Parameter(ParameterSetName = 'List')]
       [ValidateSet('manual', 'individualCI', 'batchedCI', 'schedule', 'userCreated', 'validateShelveset', 'checkInShelveset', 'triggered', 'all')]
       [string] $ReasonFilter,
 
-      [Parameter(ParameterSetName='List')]
+      [Parameter(ParameterSetName = 'List')]
       [ValidateSet('inProgress', 'completed', 'cancelling', 'postponed', 'notStarted', 'all')]
       [string] $StatusFilter,
 
-      [Parameter(ParameterSetName='List')]
+      [Parameter(ParameterSetName = 'List')]
       [int[]] $Queues,
 
-      [Parameter(ParameterSetName='List')]
+      [Parameter(ParameterSetName = 'List')]
       [int[]] $Definitions,
 
-      [Parameter(ParameterSetName='List')]
+      [Parameter(ParameterSetName = 'List')]
       [string] $BuildNumber,
 
-      [Parameter(ParameterSetName='List')]
+      [Parameter(ParameterSetName = 'List')]
       [ValidateSet('build', 'xaml')]
       [string] $Type,
 
-      [Parameter(ParameterSetName='List')]
+      [Parameter(ParameterSetName = 'List')]
       [int] $MaxBuildsPerDefinition,
 
-      [Parameter(ParameterSetName='List')]
+      [Parameter(ParameterSetName = 'List')]
       [string[]] $Properties,
 
-      [Parameter(ParameterSetName='ByID', ValueFromPipeline=$true)]
+      [Parameter(ParameterSetName = 'ByID', ValueFromPipeline = $true)]
       [Alias('BuildID')]
       [int[]] $Id
    )
@@ -107,17 +117,19 @@ function Get-Build {
             $listurl = _buildURL -projectName $ProjectName -id $item
 
             # Call the REST API
-			if (_useWindowsAuthenticationOnPremise) {
-              $resp = Invoke-RestMethod -UserAgent (_getUserAgent) -Uri $listurl -UseDefaultCredentials
-            } else {
-              $resp = Invoke-RestMethod -UserAgent (_getUserAgent) -Uri $listurl -Headers @{Authorization = "Basic $env:TEAM_PAT"}
-	        }
+            if (_useWindowsAuthenticationOnPremise) {
+               $resp = Invoke-RestMethod -UserAgent (_getUserAgent) -Uri $listurl -UseDefaultCredentials
+            }
+            else {
+               $resp = Invoke-RestMethod -UserAgent (_getUserAgent) -Uri $listurl -Headers @{Authorization = "Basic $env:TEAM_PAT"}
+            }
 
             _applyTypes -item $resp
 
             Write-Output $resp
          }
-      } else {
+      }
+      else {
          # Build the url to list the builds
          $listurl = _buildURL -projectName $ProjectName
 
@@ -134,14 +146,95 @@ function Get-Build {
          $listurl += _appendQueryString -name "definitions" -value ($definitions -join ',')
 
          # Call the REST API
-	     if (_useWindowsAuthenticationOnPremise) {
+         if (_useWindowsAuthenticationOnPremise) {
             $resp = Invoke-RestMethod -UserAgent (_getUserAgent) -Uri $listurl -UseDefaultCredentials
-         } else {
+         }
+         else {
             $resp = Invoke-RestMethod -UserAgent (_getUserAgent) -Uri $listurl -Headers @{Authorization = "Basic $env:TEAM_PAT"}
-	     }
+         }
 
          # Apply a Type Name so we can use custom format view and custom type extensions
-         foreach($item in $resp.value) {
+         foreach ($item in $resp.value) {
+            _applyTypes -item $item
+         }
+
+         Write-Output $resp.value
+      }
+   }
+}
+
+function Get-BuildLog {
+   [CmdletBinding(DefaultParameterSetName = 'ByID')]
+   param (
+      [Parameter(ParameterSetName = 'ByID', ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+      [Alias('BuildID')]
+      [int[]] $Id
+   )
+
+   DynamicParam {
+      _buildProjectNameDynamicParam
+   }
+
+   process {
+      # Bind the parameter to a friendly variable
+      $ProjectName = $PSBoundParameters["ProjectName"]
+
+      if ($id) {
+         foreach ($item in $id) {
+            # Build the url to return the logs of the build
+            $listurl = _buildURL -projectName $ProjectName -id $item -Logs
+
+            # Call the REST API to get the number of logs for the build
+            if (_useWindowsAuthenticationOnPremise) {
+               $resp = Invoke-RestMethod -UserAgent (_getUserAgent) -Uri $listurl -UseDefaultCredentials
+            }
+            else {
+               $resp = Invoke-RestMethod -UserAgent (_getUserAgent) -Uri $listurl -Headers @{Authorization = "Basic $env:TEAM_PAT"}
+            }
+
+            $fullLogIndex = $($resp.count - 1)
+
+            # Now call REST API with the index for the fullLog
+            # Build the url to return the single build
+            $listurl = _buildURL -projectName $ProjectName -id $item -Logs -LogIndex $fullLogIndex
+
+            # Call the REST API to get the number of logs for the build
+            if (_useWindowsAuthenticationOnPremise) {
+               $resp = Invoke-RestMethod -UserAgent (_getUserAgent) -Uri $listurl -UseDefaultCredentials
+            }
+            else {
+               $resp = Invoke-RestMethod -UserAgent (_getUserAgent) -Uri $listurl -Headers @{Authorization = "Basic $env:TEAM_PAT"}
+            }
+
+            Write-Output $resp.value
+         }
+      }
+      else {
+         # Build the url to list the builds
+         $listurl = _buildURL -projectName $ProjectName
+
+         $listurl += _appendQueryString -name "`$top" -value $top
+         $listurl += _appendQueryString -name "type" -value $type
+         $listurl += _appendQueryString -name "buildNumber" -value $buildNumber
+         $listurl += _appendQueryString -name "resultFilter" -value $resultFilter
+         $listurl += _appendQueryString -name "statusFilter" -value $statusFilter
+         $listurl += _appendQueryString -name "reasonFilter" -value $reasonFilter
+         $listurl += _appendQueryString -name "maxBuildsPerDefinition" -value $maxBuildsPerDefinition
+
+         $listurl += _appendQueryString -name "queues" -value ($queues -join ',')
+         $listurl += _appendQueryString -name "properties" -value ($properties -join ',')
+         $listurl += _appendQueryString -name "definitions" -value ($definitions -join ',')
+
+         # Call the REST API
+         if (_useWindowsAuthenticationOnPremise) {
+            $resp = Invoke-RestMethod -UserAgent (_getUserAgent) -Uri $listurl -UseDefaultCredentials
+         }
+         else {
+            $resp = Invoke-RestMethod -UserAgent (_getUserAgent) -Uri $listurl -Headers @{Authorization = "Basic $env:TEAM_PAT"}
+         }
+
+         # Apply a Type Name so we can use custom format view and custom type extensions
+         foreach ($item in $resp.value) {
             _applyTypes -item $item
          }
 
@@ -162,7 +255,8 @@ function Add-Build {
       if ($Global:PSDefaultParameterValues["*:projectName"]) {
          $buildDefs = Get-BuildDefinition -ProjectName $Global:PSDefaultParameterValues["*:projectName"]
          $arrSet = $buildDefs.name
-      } else {
+      }
+      else {
          Write-Verbose 'Call Set-DefaultProject for Tab Complete of BuildDefinition'
          $buildDefs = $null
          $arrSet = $null
@@ -172,10 +266,11 @@ function Add-Build {
       $rp = _buildDynamicParam -ParameterName $ParameterName -arrSet $arrSet
       $dp.Add($ParameterName, $rp)
 
-      if($Global:PSDefaultParameterValues["*:projectName"]) {
+      if ($Global:PSDefaultParameterValues["*:projectName"]) {
          $queues = Get-Queue -ProjectName $Global:PSDefaultParameterValues["*:projectName"]
          $arrSet = $queues.name
-      } else {
+      }
+      else {
          Write-Verbose 'Call Set-DefaultProject for Tab Complete of QueueName'
          $queues = $null
          $arrSet = $null
@@ -199,19 +294,20 @@ function Add-Build {
 
       # Find the BuildDefinition id from the name
       $id = Get-BuildDefinition -ProjectName "$ProjectName" -Type All |
-      Where-Object { $_.name -eq $BuildDefinition } |
-      Select-Object -ExpandProperty id
+         Where-Object { $_.name -eq $BuildDefinition } |
+         Select-Object -ExpandProperty id
 
       $body = '{"definition": {"id": ' + $id + '}}'
 
       Write-Verbose $body
 
       # Call the REST API
-	  if (_useWindowsAuthenticationOnPremise) {
-        $resp = Invoke-RestMethod -UserAgent (_getUserAgent) -Method Post -ContentType "application/json" -Body $body -Uri $listurl -UseDefaultCredentials
-      } else {
-        $resp = Invoke-RestMethod -UserAgent (_getUserAgent) -Method Post -ContentType "application/json" -Body $body -Uri $listurl -Headers @{Authorization = "Basic $env:TEAM_PAT"}
-	  }
+      if (_useWindowsAuthenticationOnPremise) {
+         $resp = Invoke-RestMethod -UserAgent (_getUserAgent) -Method Post -ContentType "application/json" -Body $body -Uri $listurl -UseDefaultCredentials
+      }
+      else {
+         $resp = Invoke-RestMethod -UserAgent (_getUserAgent) -Method Post -ContentType "application/json" -Body $body -Uri $listurl -Headers @{Authorization = "Basic $env:TEAM_PAT"}
+      }
 
       _applyTypes -item $resp
 
@@ -220,9 +316,9 @@ function Add-Build {
 }
 
 function Remove-Build {
-   [CmdletBinding(SupportsShouldProcess=$true, ConfirmImpact="High")]
+   [CmdletBinding(SupportsShouldProcess = $true, ConfirmImpact = "High")]
    param(
-      [parameter(Mandatory=$true, ValueFromPipeline=$true, ValueFromPipelineByPropertyName=$true)]
+      [parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
       [int[]] $Id,
 
       [switch] $Force
@@ -243,11 +339,12 @@ function Remove-Build {
          if ($Force -or $pscmdlet.ShouldProcess($item, "Delete Build")) {
             try {
                # Call the REST API
-	           if (_useWindowsAuthenticationOnPremise) {
-                 $resp = Invoke-RestMethod -UserAgent (_getUserAgent) -Method Delete -Uri $listurl -UseDefaultCredentials
-               } else {
-                 $resp = Invoke-RestMethod -UserAgent (_getUserAgent) -Method Delete -Uri $listurl -Headers @{Authorization = "Basic $env:TEAM_PAT"}
-	           }
+               if (_useWindowsAuthenticationOnPremise) {
+                  $resp = Invoke-RestMethod -UserAgent (_getUserAgent) -Method Delete -Uri $listurl -UseDefaultCredentials
+               }
+               else {
+                  $resp = Invoke-RestMethod -UserAgent (_getUserAgent) -Method Delete -Uri $listurl -Headers @{Authorization = "Basic $env:TEAM_PAT"}
+               }
 
                Write-Output "Deleted build $item"
             }
@@ -259,4 +356,4 @@ function Remove-Build {
    }
 }
 
-Export-ModuleMember -Alias * -Function Add-Build, Get-Build, Remove-Build
+Export-ModuleMember -Alias * -Function Add-Build, Get-Build, Remove-Build, Get-BuildLog
