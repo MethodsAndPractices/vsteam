d04fc4cc082430b75aa8aa5793506d0adce766fd
Donovan Brown <dbrown@microsoft.com>
2017-11-25 11:16:14 -0600

    BuildDefinitions has 100% code coverage

--- unit/test/buildDefinitions.Tests.ps1
+++ unit/test/buildDefinitions.Tests.ps1
@@ -8,6 +8,17 @@ Import-Module $PSScriptRoot\..\..\src\buildDefinitions.psm1 -Force
 InModuleScope buildDefinitions {
    $VSTeamVersionTable.Account = 'https://test.visualstudio.com'
 
+   $results = [PSCustomObject]@{
+      value = [PSCustomObject]@{
+         queue      = [PSCustomObject]@{ name = 'Default' }
+         _links     = [PSCustomObject]@{}
+         variables  = [PSCustomObject]@{}
+         repository = [PSCustomObject]@{}
+         project    = [PSCustomObject]@{ name = 'project' }
+         authoredBy = [PSCustomObject]@{ name = 'test'}
+      }
+   }
+
    Describe 'BuildDefinitions' {
       . "$PSScriptRoot\mockProjectNameDynamicParamNoPSet.ps1"
 
@@ -30,6 +41,28 @@ InModuleScope buildDefinitions {
             Show-VSTeamBuildDefinition -projectName project -Type Mine
 
             Assert-MockCalled _openOnWindows -Exactly -Scope It -Times 1 -ParameterFilter { $command -eq 'https://test.visualstudio.com/project/_build/index?_a=mine&path=%5c' }
+         }
+      }
+
+      Context 'Show-VSTeamBuildDefinition XAML' {
+         Mock _openOnWindows { }
+         Mock _isOnWindows { return $true }
+
+         it 'should return url for XAML' {
+            Show-VSTeamBuildDefinition -projectName project -Type XAML
+
+            Assert-MockCalled _openOnWindows -Exactly -Scope It -Times 1 -ParameterFilter { $command -eq 'https://test.visualstudio.com/project/_build/xaml&path=%5c' }
+         }
+      }
+
+      Context 'Show-VSTeamBuildDefinition Queued' {
+         Mock _openOnWindows { }
+         Mock _isOnWindows { return $true }
+
+         it 'should return url for Queued' {
+            Show-VSTeamBuildDefinition -projectName project -Type Queued
+
+            Assert-MockCalled _openOnWindows -Exactly -Scope It -Times 1 -ParameterFilter { $command -eq 'https://test.visualstudio.com/project/_build/index?_a=queued&path=%5c' }
          }
       }
 
@@ -56,14 +89,8 @@ InModuleScope buildDefinitions {
       }
 
       Context 'Get-VSTeamBuildDefinition with no parameters' {
-         Mock Invoke-RestMethod { return @{
-               value=@{
-                  queue=@{}
-                  _links=@{}
-                  project=@{}
-                  authoredBy=@{}
-               }
-            }}
+         Mock _useWindowsAuthenticationOnPremise { return $true }
+         Mock Invoke-RestMethod { return $results }
 
          It 'should return build definitions' {
             Get-VSTeamBuildDefinition -projectName project
@@ -73,14 +100,7 @@ InModuleScope buildDefinitions {
       }
 
       Context 'Get-VSTeamBuildDefinition with type parameter' {
-         Mock Invoke-RestMethod { return @{
-               value=@{
-                  queue=@{}
-                  _links=@{}
-                  project=@{}
-                  authoredBy=@{}
-               }
-            }}
+         Mock Invoke-RestMethod { return $results }
 
          It 'should return build definitions by type' {
             Get-VSTeamBuildDefinition -projectName project -type build
@@ -90,14 +110,7 @@ InModuleScope buildDefinitions {
       }
 
       Context 'Get-VSTeamBuildDefinition with filter parameter' {
-         Mock Invoke-RestMethod { return @{
-               value=@{
-                  queue=@{}
-                  _links=@{}
-                  project=@{}
-                  authoredBy=@{}
-               }
-            }}
+         Mock Invoke-RestMethod { return $results }
 
          It 'should return build definitions by filter' {
             Get-VSTeamBuildDefinition -projectName project -filter 'click*'
@@ -107,14 +120,7 @@ InModuleScope buildDefinitions {
       }
 
       Context 'Get-VSTeamBuildDefinition with both parameters' {
-         Mock Invoke-RestMethod { return @{
-               value=@{
-                  queue=@{}
-                  _links=@{}
-                  project=@{}
-                  authoredBy=@{}
-               }
-            }}
+         Mock Invoke-RestMethod { return $results }
 
          It 'should return build definitions by filter' {
             Get-VSTeamBuildDefinition -projectName project -filter 'click*' -type build
@@ -124,34 +130,47 @@ InModuleScope buildDefinitions {
       }
 
       Context 'Add-VSTeamBuildDefinition' {
-         Mock Invoke-RestMethod { return @{
-               value=@{
-                  queue=@{}
-                  _links=@{}
-                  project=@{}
-                  authoredBy=@{}
-               }
-            }}
+         Mock Invoke-RestMethod { return $results }
 
          it 'Should add build' {
             Add-VSTeamBuildDefinition -projectName project -inFile 'builddef.json'
 
             Assert-MockCalled Invoke-RestMethod -Exactly -Scope It -Times 1 -ParameterFilter {
                $Uri -eq "https://test.visualstudio.com/project/_apis/build/definitions?api-version=$($VSTeamVersionTable.Build)" -and `
-               $InFile -eq 'builddef.json' -and `
-               $Method -eq 'Post'
+                  $InFile -eq 'builddef.json' -and `
+                  $Method -eq 'Post'
             }
          }
       }
 
       Context 'Get-VSTeamBuildDefinition by ID' {
          Mock Invoke-RestMethod { return @{
-               queue=@{}
-               _links=@{}
-               project=@{}
-               variables=@{}
-               repository=@{}
-               authoredBy=@{}
+               queue      = @{}
+               _links     = @{}
+               project    = @{}
+               variables  = @{}
+               repository = @{}
+               authoredBy = @{}
+            }}
+
+         It 'should return build definition' {
+            Get-VSTeamBuildDefinition -projectName project -id 15
+
+            Assert-MockCalled Invoke-RestMethod -Exactly -Scope It -Times 1 -ParameterFilter {
+               $Uri -eq "https://test.visualstudio.com/project/_apis/build/definitions/15?api-version=$($VSTeamVersionTable.Build)"
+            }
+         }
+      }
+
+      Context 'Get-VSTeamBuildDefinition by ID local auth' {
+         Mock _useWindowsAuthenticationOnPremise { return $true }
+         Mock Invoke-RestMethod { return [PSCustomObject]@{
+               queue      = [PSCustomObject]@{}
+               _links     = [PSCustomObject]@{}
+               project    = [PSCustomObject]@{}
+               variables  = [PSCustomObject]@{}
+               repository = [PSCustomObject]@{}
+               authoredBy = [PSCustomObject]@{}
             }}
 
          It 'should return build definition' {
@@ -165,10 +184,10 @@ InModuleScope buildDefinitions {
 
       Context 'Get-VSTeamBuildDefinition with revision parameter' {
          Mock Invoke-RestMethod { return @{
-               queue=@{}
-               _links=@{}
-               project=@{}
-               authoredBy=@{}
+               queue      = @{}
+               _links     = @{}
+               project    = @{}
+               authoredBy = @{}
             }}
 
          It 'should return build definitions by revision' {
@@ -179,19 +198,50 @@ InModuleScope buildDefinitions {
       }
 
       Context 'Remove-VSTeamBuildDefinition' {
-         Mock Invoke-RestMethod { return @{
-               value=@{
-                  queue=@{}
-                  _links=@{}
-                  project=@{}
-                  authoredBy=@{}
-               }
-            }}
+         Mock Invoke-RestMethod { return $results }
 
          It 'should delete build definition' {
             Remove-VSTeamBuildDefinition -projectName project -id 2 -Force
 
             Assert-MockCalled Invoke-RestMethod -Exactly -Scope It -Times 1 -ParameterFilter { $Method -eq 'Delete' -and $Uri -eq "https://test.visualstudio.com/project/_apis/build/definitions/2?api-version=$($VSTeamVersionTable.Build)" }
+         }
+      }
+
+      # Make sure these test run last as the need differnt 
+      # $VSTeamVersionTable.Account values
+      Context 'Get-VSTeamBuildDefinition with no account' {
+         $VSTeamVersionTable.Account = $null
+
+         It 'should return build definitions' {
+            { Get-VSTeamBuildDefinition -projectName project } | Should Throw
+         }
+      }
+
+      Context 'Add-VSTeamBuildDefinition on TFS local Auth' {
+         Mock Invoke-RestMethod { return $results }
+         Mock _useWindowsAuthenticationOnPremise { return $true }
+         $VSTeamVersionTable.Account = 'http://localhost:8080/tfs/defaultcollection'
+
+         it 'Should add build' {
+            Add-VSTeamBuildDefinition -projectName project -inFile 'builddef.json'
+
+            Assert-MockCalled Invoke-RestMethod -Exactly -Scope It -Times 1 -ParameterFilter {
+               $Uri -eq "http://localhost:8080/tfs/defaultcollection/project/_apis/build/definitions?api-version=$($VSTeamVersionTable.Build)" -and `
+                  $InFile -eq 'builddef.json' -and `
+                  $Method -eq 'Post'
+            }
+         }
+      }
+
+      Context 'Remove-VSTeamBuildDefinition on TFS local Auth' {
+         Mock Invoke-RestMethod { return $results }
+         Mock _useWindowsAuthenticationOnPremise { return $true }
+         $VSTeamVersionTable.Account = 'http://localhost:8080/tfs/defaultcollection'
+
+         Remove-VSTeamBuildDefinition -projectName project -id 2 -Force
+         
+         It 'should delete build definition' {
+            Assert-MockCalled Invoke-RestMethod -Exactly -Scope Context -Times 1 -ParameterFilter { $Method -eq 'Delete' -and $Uri -eq "http://localhost:8080/tfs/defaultcollection/project/_apis/build/definitions/2?api-version=$($VSTeamVersionTable.Build)" }
          }
       }
    }
