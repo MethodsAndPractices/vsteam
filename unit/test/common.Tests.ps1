fe5bd2916eba41431671e965124abc6b59c4d803
Donovan Brown <dbrown@microsoft.com>
2017-11-27 02:16:01 -0600

    More common tests

--- unit/test/common.Tests.ps1
+++ unit/test/common.Tests.ps1
@@ -11,6 +11,53 @@ Describe 'Common' {
       }
    }
 
+   Context '_openOnWindows'{
+      Mock Start-Process -Verifiable -ParameterFilter { $FilePath -eq 'http://test.visualstudio.com' }
+      Mock Start-Process { throw "Start-Process called incorrectly: $Args" }
+
+      _openOnWindows 'http://test.visualstudio.com'
+
+      It 'should call start process' {
+         Assert-VerifiableMocks
+      }
+   }
+
+   Context '_openOnMac'{
+      Mock Start-Process -Verifiable -ParameterFilter { $FilePath -eq 'open' -and $ArgumentList -eq 'http://test.visualstudio.com' }
+      Mock Start-Process { throw "Start-Process called incorrectly: $Args" }
+
+      _openOnMac 'http://test.visualstudio.com'
+
+      It 'should call start process' {
+         Assert-VerifiableMocks
+      }
+   }
+
+   Context '_openOnLinux'{
+      Mock Start-Process -Verifiable -ParameterFilter { $FilePath -eq 'xdg-open' -and $ArgumentList -eq 'http://test.visualstudio.com' }
+      Mock Start-Process { throw "Start-Process called incorrectly: $Args" }
+
+      _openOnLinux 'http://test.visualstudio.com'
+
+      It 'should call start process' {
+         Assert-VerifiableMocks
+      }
+   }
+
+   Context '_isOnMac' {
+      It 'using IsOSX should return false' {
+         Remove-Item variable:IsMacOS
+         $global:IsOSX = $false
+         _isOnMac | Should Be $false
+      }
+
+      It 'using IsMacOS should return false' {
+         Remove-Item variable:IsOSX
+         $global:IsMacOS = $false
+         _isOnMac | Should Be $false
+      }
+   }
+
    Context '_isVSTS' {     
       It 'should return true' {
          _isVSTS 'https://test.visualstudio.com' | Should Be $true
