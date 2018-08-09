d4187105ab2c2a69f31937400a4aae006395a2ce
DarqueWarrior <dlb@dlbonline.com>
2018-08-09 02:40:27 -0500

    Removed Open On functions

--- src/common.ps1
+++ src/common.ps1
@@ -135,70 +135,7 @@ function _getVSTeamAPIVersion {
 }
 
 function _isOnWindows {
-   # This will work on 6.0 and later but is missing on
-   # older versions
-   if (Test-Path -Path 'variable:global:IsWindows') {
-      return Get-Content -Path 'variable:global:IsWindows'
-   }
-   # This should catch older versions
-   elseif (Test-Path -Path 'env:os') {
-      return (Get-Content -Path 'env:os').StartsWith("Windows")
-   }
-   # If all else fails
-   else {
-      return $false
-   }
-}
-
-function _isOnLinux {
-   if (Test-Path -Path 'variable:global:IsLinux') {
-      return Get-Content -Path 'variable:global:IsLinux'
-   }
-
-   return $false
-}
-
-function _isOnMac {
-   # The variable to test if you are on Mac OS changed from
-   # IsOSX to IsMacOS. Because I have Set-StrictMode -Version Latest
-   # trying to access a variable that is not set will crash.
-   # So I use Test-Path to determine which exist and which to use.
-   if (Test-Path -Path 'variable:global:IsMacOS') {
-      return Get-Content -Path 'variable:global:IsMacOS'
-   }
-   elseif (Test-Path -Path 'variable:global:IsOSX') {
-      return Get-Content -Path 'variable:global:IsOSX'
-   }
-   else {
-      return $false
-   }
-}
-
-function _openOnWindows {
-   param(
-      [parameter(Mandatory = $true)]
-      [string] $command
-   )
-
-   Start-Process "$command"
-}
-
-function _openOnMac {
-   param(
-      [parameter(Mandatory = $true)]
-      [string] $command
-   )
-
-   Start-Process -FilePath open -Args "$command"
-}
-
-function _openOnLinux {
-   param(
-      [parameter(Mandatory = $true)]
-      [string] $command
-   )
-
-   Start-Process -FilePath xdg-open -Args "$command"
+   return Get-OperatingSystem -eq 'Windows'
 }
 
 function _addSubDomain {
@@ -242,17 +179,7 @@ function _getUserAgent {
    [CmdletBinding()]
    param()
 
-   $os = 'unknown'
-
-   if (_isOnWindows) {
-      $os = 'Windows'
-   }
-   elseif (_isOnMac) {
-      $os = 'OSX'
-   }
-   elseif (_isOnLinux) {
-      $os = 'Linux'
-   }
+   $os = Get-OperatingSystem
 
    $result = "Team Module/$($VSTeamVersionTable.ModuleVersion) ($os) PowerShell/$($PSVersionTable.PSVersion.ToString())"
 
--- unit/test/common.Tests.ps1
+++ unit/test/common.Tests.ps1
@@ -29,15 +29,13 @@ Describe 'Common' {
    }
 
    Context '_getUserAgent on Mac' {
-      Mock _isOnWindows { return $false }
-      Mock _isOnMac { return $true }
-      Mock _isOnLinux { return $false }
+      Mock Get-OperatingSystem { return 'macOS' }
       $VSTeamVersionTable.ModuleVersion = '0.0.0'
 
       $actual = _getUserAgent
 
-      It 'Should return User Agent for OSx' {
-         $actual | Should BeLike '*OSX*'
+      It 'Should return User Agent for macOS' {
+         $actual | Should BeLike '*macOS*'
       }
 
       It 'Should return User Agent for Module Version' {
@@ -50,9 +48,7 @@ Describe 'Common' {
    }
 
    Context '_getUserAgent on Linux' {
-      Mock _isOnWindows { return $false }
-      Mock _isOnMac { return $false }
-      Mock _isOnLinux { return $true }
+      Mock Get-OperatingSystem { return 'Linux' }
       $VSTeamVersionTable.ModuleVersion = '0.0.0'
 
       $actual = _getUserAgent
@@ -111,126 +107,6 @@ Describe 'Common' {
 
          _handleException $ex
 
-         Assert-VerifiableMock
-      }
-   }
-
-   Context '_openOnWindows' {
-      Mock Start-Process -Verifiable -ParameterFilter { $FilePath -eq 'http://test.visualstudio.com' }
-      Mock Start-Process { throw "Start-Process called incorrectly: $Args" }
-
-      _openOnWindows 'http://test.visualstudio.com'
-
-      It 'should call start process' {
-         Assert-VerifiableMock
-      }
-   }
-
-   Context '_openOnMac' {
-      Mock Start-Process -Verifiable -ParameterFilter { $FilePath -eq 'open' -and $ArgumentList -eq 'http://test.visualstudio.com' }
-      Mock Start-Process { throw "Start-Process called incorrectly: $Args" }
-
-      _openOnMac 'http://test.visualstudio.com'
-
-      It 'should call start process' {
-         Assert-VerifiableMock
-      }
-   }
-
-   Context '_openOnLinux' {
-      Mock Start-Process -Verifiable -ParameterFilter { $FilePath -eq 'xdg-open' -and $ArgumentList -eq 'http://test.visualstudio.com' }
-      Mock Start-Process { throw "Start-Process called incorrectly: $Args" }
-
-      _openOnLinux 'http://test.visualstudio.com'
-
-      It 'should call start process' {
-         Assert-VerifiableMock
-      }
-   }
-
-   Context '_isOnLinux' {
-      It 'on PowerShell 5.* should return false' {
-         Mock Test-Path { return $false }
-
-         _isOnLinux | Should Be $false
-      }
-
-      It 'using IsLinux should return true' {
-         Mock Test-Path { return $true } -ParameterFilter { $Path -eq 'variable:global:IsLinux'} -Verifiable
-         Mock Get-Content { return $true } -ParameterFilter { $Path -eq 'variable:global:IsLinux'} -Verifiable
-
-         Mock Test-Path { throw "Wrong call to Test-Path: $args" }
-         Mock Get-Content { throw "Wrong call to Get-Content: $args" }
-
-         _isOnLinux | Should Be $true
-         Assert-VerifiableMock
-      }
-   }
-
-   Context '_isOnWindows on PowerShell 5.* ' {
-      Mock Test-Path { return $false } { $Path -eq 'variable:global:IsWindows'} -Verifiable
-      Mock Test-Path { return $true } { $Path -eq 'env:os'} -Verifiable
-      Mock Get-Content { return 'Windows_NT' } -ParameterFilter { $Path -eq 'env:os'} -Verifiable
-
-      Mock Test-Path { throw "Wrong call to Test-Path: $args" }
-      Mock Get-Content { throw "Wrong call to Get-Content: $args" }
-
-      It 'should return true' {
-         _isOnWindows | Should Be $true
-         Assert-VerifiableMock
-      }
-   }
-
-   Context '_isOnWindows using IsWindows ' {
-      Mock Test-Path { return $true } -ParameterFilter { $Path -eq 'variable:global:IsWindows'} -Verifiable
-      Mock Get-Content { return $true } -ParameterFilter { $Path -eq 'variable:global:IsWindows'} -Verifiable
-
-      Mock Test-Path { throw "Wrong call to Test-Path: $args" }
-      Mock Get-Content { throw "Wrong call to Get-Content: $args" }
-
-      It 'should return true' {
-         _isOnWindows | Should Be $true
-         Assert-VerifiableMock
-      }
-   }
-
-   Context '_isOnWindows' {
-      Mock Test-Path { return $false } -Verifiable
-      Mock Get-Content { throw "Wrong call to Get-Content: $args" }
-
-      It 'using IsWindows should return false' {
-         _isOnWindows | Should Be $false
-         Assert-VerifiableMock
-      }
-   }
-
-   Context '_isOnMac' {
-      It 'on PowerShell 5.* should return false' {
-         Mock Test-Path { return $false }
-
-         _isOnMac | Should Be $false
-      }
-
-      It 'using IsMacOS should return true' {
-         Mock Test-Path { return $true } -ParameterFilter { $Path -eq 'variable:global:IsMacOS'} -Verifiable
-         Mock Get-Content { return $true } -ParameterFilter { $Path -eq 'variable:global:IsMacOS'} -Verifiable
-
-         Mock Test-Path { throw "Wrong call to Test-Path: $args" }
-         Mock Get-Content { throw "Wrong call to Get-Content: $args" }
-
-         _isOnMac | Should Be $true
-         Assert-VerifiableMock
-      }
-
-      It 'using IsOSX should return false' {
-         Mock Test-Path { return $false } -ParameterFilter { $Path -eq 'variable:global:IsMacOS'} -Verifiable
-         Mock Test-Path { return $true } -ParameterFilter { $Path -eq 'variable:global:IsOSX'} -Verifiable
-         Mock Get-Content { return $false } -ParameterFilter { $Path -eq 'variable:global:IsOSX'} -Verifiable
-
-         Mock Test-Path { throw "Wrong call to Test-Path: $args" }
-         Mock Get-Content { throw "Wrong call to Get-Content: $args" }
-
-         _isOnMac | Should Be $false
          Assert-VerifiableMock
       }
    }
