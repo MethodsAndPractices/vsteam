e856aef1ebc8e29be4bc0decd99c9ea52df07283
Kai Walter <kai.walter@zeiss.com>
2018-09-08 20:05:24 +0200

    check build definition type after add in integration test

--- integration/test/010_projects.Tests.ps1
+++ integration/test/010_projects.Tests.ps1
@@ -203,6 +203,16 @@ Describe 'VSTeam Integration Tests' -Tag 'integration' {
          $buildDefs.Count | Should Be 2
       }
 
+      It 'Get-VSTeamBuildDefinition by Type "build" should return 2 build definitions' {
+         $buildDefs = Get-VSTeamBuildDefinition -ProjectName $newProjectName -Type build
+         $buildDefs.Count | Should Be 2
+      }
+
+      It 'Get-VSTeamBuildDefinition by Type "xaml" should return no build definitions' {
+         $buildDefs = Get-VSTeamBuildDefinition -ProjectName $newProjectName -Type xaml
+         $buildDefs.Count | Should Be 0
+      }
+
       # Only run for VSTS
       if ($api -eq 'VSTS') {
          It 'Get-VSTeamBuildDefinition by Id should return intended attribute values for 1st build definition' {
