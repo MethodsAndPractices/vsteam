f78acbbc84379f2011f5a25148bc9838462a4b93
Donovan Brown <dlb@dlbonline.com>
2018-09-16 06:17:37 -0400

    Fixing issue #90 (#96)
    Set top to 10,000 when searching by email
    Updated help for Update-VSTeamUser
    Fixed issue where api version was not being set on load.

--- src/teamspsdrive.ps1
+++ src/teamspsdrive.ps1
@@ -65,7 +65,7 @@ using namespace Microsoft.PowerShell.SHiPS
 class VSTeamVersions {
    static [string] $Account = $env:TEAM_ACCT
    static [string] $DefaultProject = $env:TEAM_PROJECT
-   static [string] $Version = $env:TEAM_VERSION
+   static [string] $Version = $(If ($env:TEAM_VERSION) {$env:TEAM_VERSION} Else {"TFS2017"})
    static [string] $Build = '3.0'
    static [string] $Release = '3.0-preview'
    static [string] $Core = '3.0'
