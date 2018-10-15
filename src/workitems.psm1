d5508fd9451d96006f0d029cc58c0e1c18267272
Richard Diphoorn <rdtechie@users.noreply.github.com>
2018-10-16 00:18:44 +0200

    Added properties on Workitem (#108)
    * Added more properties to be updated on a workitem.
    
    Added 3 additional properties on a workitem; description, iterationpath & assignedto.
    
    * Updated parameters
    
    Removed the unecessary empty strings on the parameters.

--- src/workitems.psm1
+++ src/workitems.psm1
@@ -7,141 +7,170 @@ $here = Split-Path -Parent $MyInvocation.MyCommand.Path
 # Apply types to the returned objects so format and type files can
 # identify the object and act on it.
 function _applyTypesToWorkItem {
-   param($item)
+    param($item)
 
-   # If there are ids in the list that don't map to a work item and empty
-   # entry is returned in its place if ErrorPolicy is Omit.
-   if ($item) {
-      $item.PSObject.TypeNames.Insert(0, 'Team.WorkItem')
-   }
+    # If there are ids in the list that don't map to a work item and empty
+    # entry is returned in its place if ErrorPolicy is Omit.
+    if ($item) {
+        $item.PSObject.TypeNames.Insert(0, 'Team.WorkItem')
+    }
 }
 
 function Add-VSTeamWorkItem {
-   [CmdletBinding()]
-   param(
-      [Parameter(Mandatory = $true)]
-      [string]$Title
-   )
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $true)]
+        [string]$Title,
 
-   DynamicParam {
-      $dp = _buildProjectNameDynamicParam -mandatory $true
+        [Parameter(Mandatory = $false)]
+        [string]$Description,
 
-      # If they have not set the default project you can't find the
-      # validateset so skip that check. However, we still need to give
-      # the option to pass a WorkItemType to use.
-      if ($Global:PSDefaultParameterValues["*:projectName"]) {
-         $wittypes = _getWorkItemTypes -ProjectName $Global:PSDefaultParameterValues["*:projectName"]
-         $arrSet = $wittypes
-      }
-      else {
-         Write-Verbose 'Call Set-VSTeamDefaultProject for Tab Complete of WorkItemType'
-         $wittypes = $null
-         $arrSet = $null
-      }
+        [Parameter(Mandatory = $false)]
+        [string]$IterationPath,
 
-      $ParameterName = 'WorkItemType'
-      $rp = _buildDynamicParam -ParameterName $ParameterName -arrSet $arrSet
-      $dp.Add($ParameterName, $rp)
+        [Parameter(Mandatory = $false)]
+        [string]$AssignedTo
+    )
 
-      $dp
-   }
+    DynamicParam {
+        $dp = _buildProjectNameDynamicParam -mandatory $true
 
-   Process {
-      # Bind the parameter to a friendly variable
-      $ProjectName = $PSBoundParameters["ProjectName"]
+        # If they have not set the default project you can't find the
+        # validateset so skip that check. However, we still need to give
+        # the option to pass a WorkItemType to use.
+        if ($Global:PSDefaultParameterValues["*:projectName"]) {
+            $wittypes = _getWorkItemTypes -ProjectName $Global:PSDefaultParameterValues["*:projectName"]
+            $arrSet = $wittypes
+        } else {
+            Write-Verbose 'Call Set-VSTeamDefaultProject for Tab Complete of WorkItemType'
+            $wittypes = $null
+            $arrSet = $null
+        }
 
-      # The type has to start with a $
-      $WorkItemType = "`$$($PSBoundParameters["WorkItemType"])"
+        $ParameterName = 'WorkItemType'
+        $rp = _buildDynamicParam -ParameterName $ParameterName -arrSet $arrSet
+        $dp.Add($ParameterName, $rp)
 
-      $body = '[{"op": "add", "path": "/fields/System.Title", "from": null, "value": "' + $Title + '"}]'
+        $dp
+    }
 
-      # Call the REST API
-      $resp = _callAPI -ProjectName $ProjectName -Area 'wit' -Resource 'workitems'  `
-         -Version $([VSTeamVersions]::Core) -id $WorkItemType -Method Post `
-         -ContentType 'application/json-patch+json' -Body $body
+    Process {
+        # Bind the parameter to a friendly variable
+        $ProjectName = $PSBoundParameters["ProjectName"]
 
-      _applyTypesToWorkItem -item $resp
+        # The type has to start with a $
+        $WorkItemType = "`$$($PSBoundParameters["WorkItemType"])"
+      
+        # Constructing the contents to be send. 
+        # Empty parameters will be skipped when converting to json.
+        $body = @(
+            @{
+                op    = "add"
+                path  = "/fields/System.Title"
+                value = $Title
+            }
+            @{
+                op    = "add"
+                path  = "/fields/System.Description"
+                value = $Description
+            }
+            @{
+                op    = "add"
+                path  = "/fields/System.IterationPath"
+                value = $IterationPath
+            }
+            @{
+                op    = "add"
+                path  = "/fields/System.AssignedTo"
+                value = $AssignedTo
+            }) | Where { $_.value} | ConvertTo-Json
 
-      return $resp
-   }
+        # Call the REST API
+        $resp = _callAPI -ProjectName $ProjectName -Area 'wit' -Resource 'workitems'  `
+            -Version $([VSTeamVersions]::Core) -id $WorkItemType -Method Post `
+            -ContentType 'application/json-patch+json' -Body $body
+
+        _applyTypesToWorkItem -item $resp
+
+        return $resp
+    }
 }
 
 function Show-VSTeamWorkItem {
-   [CmdletBinding()]
-   param(
-      [Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true, Position = 0)]
-      [Alias('WorkItemID')]
-      [int] $Id
-   )
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true, Position = 0)]
+        [Alias('WorkItemID')]
+        [int] $Id
+    )
 
-   DynamicParam {
-      _buildProjectNameDynamicParam
-   }
+    DynamicParam {
+        _buildProjectNameDynamicParam
+    }
 
-   process {
-      # Bind the parameter to a friendly variable
-      $ProjectName = $PSBoundParameters["ProjectName"]
+    process {
+        # Bind the parameter to a friendly variable
+        $ProjectName = $PSBoundParameters["ProjectName"]
 
-      Show-Browser "$([VSTeamVersions]::Account)/$ProjectName/_workitems/edit/$Id"
-   }
+        Show-Browser "$([VSTeamVersions]::Account)/$ProjectName/_workitems/edit/$Id"
+    }
 }
 
 function Get-VSTeamWorkItem {
-   [CmdletBinding(DefaultParameterSetName = 'ByID')]
-   param(
-      [Parameter(ParameterSetName = 'ByID', Mandatory = $true, ValueFromPipeline = $true, Position = 0)]
-      [int] $Id,
+    [CmdletBinding(DefaultParameterSetName = 'ByID')]
+    param(
+        [Parameter(ParameterSetName = 'ByID', Mandatory = $true, ValueFromPipeline = $true, Position = 0)]
+        [int] $Id,
 
-      [Parameter(ParameterSetName = 'List', Mandatory = $true, ValueFromPipeline = $true, Position = 0)]
-      [int[]] $Ids,
+        [Parameter(ParameterSetName = 'List', Mandatory = $true, ValueFromPipeline = $true, Position = 0)]
+        [int[]] $Ids,
 
-      [Parameter(ParameterSetName = 'List')]
-      [ValidateSet('Fail', 'Omit')]
-      [string] $ErrorPolicy = 'Fail',
+        [Parameter(ParameterSetName = 'List')]
+        [ValidateSet('Fail', 'Omit')]
+        [string] $ErrorPolicy = 'Fail',
 
-      [ValidateSet('None', 'Relations', 'Fields', 'Links', 'All')]
-      [string] $Expand = 'None',
+        [ValidateSet('None', 'Relations', 'Fields', 'Links', 'All')]
+        [string] $Expand = 'None',
 
-      [string[]] $Fields
-   )
+        [string[]] $Fields
+    )
 
-   DynamicParam {
-      _buildProjectNameDynamicParam -mandatory $true -Position 1
-   }
+    DynamicParam {
+        _buildProjectNameDynamicParam -mandatory $true -Position 1
+    }
 
-   Process {
-      # Bind the parameter to a friendly variable
-      $ProjectName = $PSBoundParameters["ProjectName"]
+    Process {
+        # Bind the parameter to a friendly variable
+        $ProjectName = $PSBoundParameters["ProjectName"]
 
-      # Call the REST API
-      if ($Ids) {
-         $resp = _callAPI -ProjectName $ProjectName -Area 'wit' -Resource 'workitems'  `
-            -Version $([VSTeamVersions]::Core) `
-            -Querystring @{
-            '$Expand'   = $Expand
-            fields      = ($Fields -join ',')
-            errorPolicy = $ErrorPolicy
-            ids         = ($ids -join ',')
-         }
+        # Call the REST API
+        if ($Ids) {
+            $resp = _callAPI -ProjectName $ProjectName -Area 'wit' -Resource 'workitems'  `
+                -Version $([VSTeamVersions]::Core) `
+                -Querystring @{
+                '$Expand'   = $Expand
+                fields      = ($Fields -join ',')
+                errorPolicy = $ErrorPolicy
+                ids         = ($ids -join ',')
+            }
 
-         foreach ($item in $resp.value) {
-            _applyTypesToWorkItem -item $item
-         }
-      }
-      else {
-         $a = $id[0]
-         $resp = _callAPI -ProjectName $ProjectName -Area 'wit' -Resource 'workitems'  `
-            -Version $([VSTeamVersions]::Core) -id "$a" `
-            -Querystring @{
-            '$Expand' = $Expand
-            fields      = ($Fields -join ',')
-         }
+            foreach ($item in $resp.value) {
+                _applyTypesToWorkItem -item $item
+            }
+        } else {
+            $a = $id[0]
+            $resp = _callAPI -ProjectName $ProjectName -Area 'wit' -Resource 'workitems'  `
+                -Version $([VSTeamVersions]::Core) -id "$a" `
+                -Querystring @{
+                '$Expand' = $Expand
+                fields    = ($Fields -join ',')
+            }
 
-         _applyTypesToWorkItem -item $resp
-      }
+            _applyTypesToWorkItem -item $resp
+        }
 
-      return $resp
-   }
+        return $resp
+    }
 }
 
 Set-Alias Add-WorkItem Add-VSTeamWorkItem
@@ -149,5 +178,5 @@ Set-Alias Get-WorkItem Get-VSTeamWorkItem
 Set-Alias Show-WorkItem Show-VSTeamWorkItem
 
 Export-ModuleMember `
-   -Function Add-VSTeamWorkItem, Get-VSTeamWorkItem, Show-VSTeamWorkItem `
-   -Alias Add-WorkItem, Get-WorkItem, Show-WorkItem
+    -Function Add-VSTeamWorkItem, Get-VSTeamWorkItem, Show-VSTeamWorkItem `
+    -Alias Add-WorkItem, Get-WorkItem, Show-WorkItem
