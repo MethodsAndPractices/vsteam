d2a52bd59319655f17c8076dc23da4f7185e73d3
Sebastian Schütze <sebastian.schuetze@razorspoint.com>
2019-03-31 21:45:31 +0200

    add Get-VSTeamBuildTime function script

---
+++ Source/Public/Get-VSTeamBuildTimeline.ps1
@@ -0,0 +1,46 @@
+function Get-VSTeamBuildTimeline {
+   [CmdletBinding(DefaultParameterSetName = 'ByID')]
+   param (
+      [Parameter(ParameterSetName = 'ByID')]
+      [Alias('TimelineId')]
+      [Guid] $Id,
+
+      [Parameter(ParameterSetName = 'ByID')]
+      [int] $ChangeId,
+
+      [Parameter(ParameterSetName = 'ByID')]
+      [Guid] $PlanId,
+
+      [Parameter(ParameterSetName = 'ByID', ValueFromPipeline = $true)]
+      [int[]] $BuildID
+   )
+
+   DynamicParam {
+      _buildProjectNameDynamicParam
+   }
+
+   process {
+      # Bind the parameter to a friendly variable
+      $ProjectName = $PSBoundParameters["ProjectName"]
+  
+      foreach ($item in $BuildID) {
+         # Build the url to return the single build
+
+         $resource = "builds/$item/timeline"
+
+         if($Id){
+            $resource = "builds/$item/timeline/$Id"
+         }
+
+         $resp = _callAPI -ProjectName $projectName -Area 'build' -Resource $resource `
+            -Version $([VSTeamVersions]::Build) `
+            -Querystring @{
+               'changeId'                 = $ChangeId
+               'planId'                   = $PlanId              
+            }
+
+         Write-Output $resp
+      }
+   
+   }
+}
