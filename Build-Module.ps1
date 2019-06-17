bad0d8d622a787b5b5ec14f2a4ce1d6395f6aac7
Donovan Brown <dbrown@microsoft.com>
2019-06-17 09:48:27 +0200

    Renamed build script Adding flag to build help

--- Build-Module.ps1
+++ Build-Module.ps1
@@ -1,7 +1,10 @@
 [CmdletBinding()]
 param(
-   [Parameter(Mandatory = $True)]
-   [string]$outputDir = './dist'
+   [string]$outputDir = './dist',
+
+   # Building help is skipped by default to speed your inner loop.
+   # Use this flag to include building the help
+   [switch]$buildHelp
 )
 
 . ./Merge-Files.ps1
@@ -19,11 +22,24 @@ Merge-Files -inputFile ./Source/Classes/_classes.json -outputDir $output
 Merge-Files -inputFile ./Source/formats/_formats.json -outputDir $output
 Merge-Files -inputFile ./Source/Private/_private.json -outputDir $output
 
+# Build the help
+if ($buildHelp.IsPresent) {
+   Write-Output 'Creating help files'
+   Push-Location
+   Set-Location ./.docs
+   ./gen-help.ps1
+   Pop-Location
+}
+
+Write-Output 'Publishing help files'
 Copy-Item -Path ./Source/en-US -Destination "$output/" -Recurse -Force
 Copy-Item -Path ./Source/VSTeam.psm1 -Destination "$output/VSTeam.psm1" -Force
 
+Write-Output 'Updating Functions To Export'
 $newValue = ((Get-ChildItem -Path "./Source/Public" -Filter '*.ps1').BaseName |
    ForEach-Object -Process { Write-Output "'$_'" }) -join ','
 
 (Get-Content "./Source/VSTeam.psd1") -Replace ("FunctionsToExport.+", "FunctionsToExport = ($newValue)") |
 Set-Content "$output/VSTeam.psd1"
+
+Write-Output 'Publish complete'
