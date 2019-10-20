939bb680a7ca2a0b8c96f79dacbc7738f33e457c
Dick van de Meulengraaf <dick.vandemeulengraaf@oce.com>
2019-10-20 18:16:49 +0200

    Do not change default (yet ;-))

--- Source/Classes/VSTeamVersions.ps1
+++ Source/Classes/VSTeamVersions.ps1
@@ -1,14 +1,14 @@
 class VSTeamVersions {
    static [string] $Account = $env:TEAM_ACCT
    static [string] $DefaultProject = $env:TEAM_PROJECT
-   static [string] $Version = $(If ($env:TEAM_VERSION) {$env:TEAM_VERSION} Else {"AzD2019"})
-   static [string] $Build = '5.0'
-   static [string] $Release = '5.0'
-   static [string] $Core = '5.0'
-   static [string] $Git = '5.0'
-   static [string] $DistributedTask = '5.0'
-   static [string] $VariableGroups = '5.0'
-   static [string] $Tfvc = '5.0'
+   static [string] $Version = $(If ($env:TEAM_VERSION) {$env:TEAM_VERSION} Else {"TFS2017"})
+   static [string] $Build = '3.0'
+   static [string] $Release = '3.0-preview'
+   static [string] $Core = '3.0'
+   static [string] $Git = '3.0'
+   static [string] $DistributedTask = '3.0-preview'
+   static [string] $VariableGroups = '3.2-preview.1'
+   static [string] $Tfvc = '3.0'
    static [string] $Packaging = ''
    static [string] $MemberEntitlementManagement = ''
    static [string] $ExtensionsManagement = ''
