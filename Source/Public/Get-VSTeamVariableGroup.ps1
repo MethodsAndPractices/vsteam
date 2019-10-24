1b1dccc3592cf50cc07cf566ce73f465b0b94de0
Louis Tourtellotte <louis.email@gmail.com>
2019-10-24 16:18:04 -0700

    revert: Get-VSTeamVarGrp _aplyTypesToVariableGroup

--- Source/Public/Get-VSTeamVariableGroup.ps1
+++ Source/Public/Get-VSTeamVariableGroup.ps1
@@ -30,8 +30,6 @@ function Get-VSTeamVariableGroup {
             $resp = _callAPI -ProjectName $ProjectName -Area 'distributedtask' -Resource 'variablegroups' -Version $([VSTeamVersions]::VariableGroups) -Method Get `
                -QueryString @{groupName = $Name}
 
-            _applyTypesToVariableGroup -item $resp
-
             Write-Output $resp
          }
          else {
