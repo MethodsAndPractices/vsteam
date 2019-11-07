37734b740b8e3dda615f4499bf3245950ccbc8d2
Donovan Brown <dbrown@microsoft.com>
2019-11-07 10:55:21 -0500

    Updated the API version code.

--- Source/Public/Set-VSTeamAPIVersion.ps1
+++ Source/Public/Set-VSTeamAPIVersion.ps1
@@ -6,7 +6,7 @@ function Set-VSTeamAPIVersion {
       [string] $Target = 'TFS2017',
 
       [parameter(ParameterSetName = 'Service', Mandatory = $true, Position = 0)]
-      [ValidateSet('Build', 'Release', 'Core', 'Git', 'DistributedTask', 'VariableGroups', 'Tfvc', 'Packaging', 'MemberEntitlementManagement', 'ExtensionsManagement', 'ServiceFabricEndpoint', 'Graph')]
+      [ValidateSet('Build', 'Release', 'Core', 'Git', 'DistributedTask', 'VariableGroups', 'Tfvc', 'Packaging', 'MemberEntitlementManagement', 'ExtensionsManagement', 'ServiceFabricEndpoint', 'Graph', 'TaskGroups')]
       [string] $Service,
 
       [parameter(ParameterSetName = 'Service', Mandatory = $true, Position = 1)]
@@ -54,6 +54,9 @@ function Set-VSTeamAPIVersion {
             'Graph' {
                [VSTeamVersions]::Graph = $Version
             }
+            'TaskGroups' {
+               [VSTeamVersions]::TaskGroups = $Version
+            }
          }
       }
       else {
@@ -68,6 +71,7 @@ function Set-VSTeamAPIVersion {
                [VSTeamVersions]::VariableGroups = '4.0-preview'
                [VSTeamVersions]::Tfvc = '3.2'
                [VSTeamVersions]::Packaging = ''
+               [VSTeamVersions]::TaskGroups = '4.0-preview'
                [VSTeamVersions]::MemberEntitlementManagement = ''
                [VSTeamVersions]::ServiceFabricEndpoint = '3.2'
                [VSTeamVersions]::ExtensionsManagement = '3.2-preview'
@@ -83,6 +87,7 @@ function Set-VSTeamAPIVersion {
                [VSTeamVersions]::VariableGroups = '3.2-preview.1'
                [VSTeamVersions]::Tfvc = '3.0'
                [VSTeamVersions]::Packaging = ''
+               [VSTeamVersions]::TaskGroups = '3.2-preview.1'
                [VSTeamVersions]::MemberEntitlementManagement = ''
                [VSTeamVersions]::ServiceFabricEndpoint = ''
                [VSTeamVersions]::ExtensionsManagement = '3.0-preview'
@@ -98,6 +103,7 @@ function Set-VSTeamAPIVersion {
                [VSTeamVersions]::VariableGroups = '5.0-preview.1'
                [VSTeamVersions]::Tfvc = '5.0'
                [VSTeamVersions]::Packaging = '5.1-preview'
+               [VSTeamVersions]::TaskGroups = '5.1-preview.1'
                [VSTeamVersions]::MemberEntitlementManagement = '5.1-preview'
                # This version is never passed to the API but is used to evaluate
                # if Service Fabric is enabled for the account. Just set it to
@@ -119,6 +125,7 @@ function Set-VSTeamAPIVersion {
    Write-Verbose "VariableGroups: $([VSTeamVersions]::VariableGroups)"
    Write-Verbose "Tfvc: $([VSTeamVersions]::Tfvc)"
    Write-Verbose "Packaging: $([VSTeamVersions]::Packaging)"
+   Write-Verbose "TaskGroups: $([VSTeamVersions]::TaskGroups)"
    Write-Verbose "MemberEntitlementManagement: $([VSTeamVersions]::MemberEntitlementManagement)"
    Write-Verbose "ServiceFabricEndpoint: $([VSTeamVersions]::ServiceFabricEndpoint)"
    Write-Verbose "ExtensionsManagement: $([VSTeamVersions]::ExtensionsManagement)"
