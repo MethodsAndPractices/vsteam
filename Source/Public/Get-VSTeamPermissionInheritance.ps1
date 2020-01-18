874eb70fcca9bdc2c548520c7cd8db2dec91078b
Donovan Brown <dbrown@microsoft.com>
2020-01-18 12:44:14 -0600

    Moved common code for reuse.

--- Source/Public/Get-VSTeamPermissionInheritance.ps1
+++ Source/Public/Get-VSTeamPermissionInheritance.ps1
@@ -1,97 +1,39 @@
 ﻿function Get-VSTeamPermissionInheritance {
-    [OutputType([System.String])]
-    [CmdletBinding()]
-    param(
-       [Parameter(Mandatory)]
-       [string] $resourceName,
-       [Parameter(Mandatory)]  
-       [ValidateSet('Repository','BuildDefinition','ReleaseDefinition')]
-       [string] $resourceType
-    )
- 
-    process 
-    {
-        if($env:TEAM_PROJECT)
-        {
-            $projectID = (Get-VSTeamProject -Name $env:TEAM_PROJECT -ErrorAction SilentlyContinue | Select-Object -ExpandProperty id -ErrorAction SilentlyContinue)
-        }
-        else
-        {
-            Write-Error "Unable to retrieve project information. Ensure that Set-VSTeamDefaultProject has been run prior to execution."
-            Return
-        }
+   [OutputType([System.String])]
+   [CmdletBinding()]
+   param(
+      [Parameter(Mandatory)]
+      [string] $resourceName,
 
-        Switch($resourceType)
-        {
-            "Repository"
-            {
-                $securityNamespaceID = "2e9eb7ed-3c0a-47d4-87c1-0ffdd275fd87"
+      [Parameter(Mandatory)]
+      [ValidateSet('Repository', 'BuildDefinition', 'ReleaseDefinition')]
+      [string] $resourceType
+   )
 
-                $repositoryID = (Get-VSTeamGitRepository -Name "$resourceName" -ProjectName "$env:TEAM_PROJECT" -WarningAction SilentlyContinue -ErrorAction SilentlyContinue | Select-Object -ExpandProperty id -ErrorAction SilentlyContinue)
+   DynamicParam {
+      _buildProjectNameDynamicParam -mandatory $true
+   }
 
-                If($null -eq $repositoryID)
-                {
-                    Write-Error "Unable to retrieve repository information. Ensure that the resourceName provided matches a repository name exactly."
-                    Return
-                }
+   process {
+      # Bind the parameter to a friendly variable
+      $ProjectName = $PSBoundParameters["ProjectName"]
+      Write-Verbose "Creating VSTeamPermissionInheritance"
+      $item = [VSTeamPermissionInheritance]::new($ProjectName, $resourceName, $resourceType)
+      $token = $item.Token
+      $version = $item.Version
+      $projectID = $item.ProjectID
+      $securityNamespaceID = $item.SecurityNamespaceID
 
-                $token = "repoV2/$projectID/$repositoryID"
+      Write-Verbose "Token = $token"
+      Write-Verbose "Version = $Version"
+      Write-Verbose "ProjectID = $ProjectID"
+      Write-Verbose "SecurityNamespaceID = $SecurityNamespaceID"
 
-                $Version = "$([VSTeamVersions]::Git)"
-
-            }
-            "BuildDefinition"
-            {
-                $securityNamespaceID = "33344d9c-fc72-4d6f-aba5-fa317101a7e9"
-
-                $buildDefinitionID = (Get-VSTeamBuildDefinition -ProjectName $env:team_project -ErrorAction SilentlyContinue | Where-Object name -eq "$resourceName" -ErrorAction SilentlyContinue | Select-Object -ExpandProperty id -ErrorAction SilentlyContinue)
-
-                If($null -eq $buildDefinitionID)
-                {
-                    Write-Error "Unable to retrieve build definition information. Ensure that the resourceName provided matches a build definition name exactly."
-                    Return
-                }
-
-                $token = "$projectID/$buildDefinitionID"
-
-                $Version = "$([VSTeamVersions]::Build)"
-            }
-
-            "ReleaseDefinition"
-            {
-                $securityNamespaceID = "c788c23e-1b46-4162-8f5e-d7585343b5de"
-
-                $releaseDefinition = (Get-VSTeamReleaseDefinition -ProjectName $env:team_project -ErrorAction SilentlyContinue | Where-Object -Property name -eq "$resourceName" -ErrorAction SilentlyContinue)
-
-                If($null -eq $releaseDefinition)
-                {
-                    Write-Error "Unable to retrieve release definition information. Ensure that the resourceName provided matches a release definition name exactly."
-                    Return
-                }
-
-                if(($releaseDefinition).path -eq "/")
-                {
-                    $token = "$projectID/$($releaseDefinition.id)"
-                }
-                else
-                {
-                    $token = "$projectID"+"$($releaseDefinition.path -replace "\\","/")"+"/$($releaseDefinition.id)"
-                }
-
-                $Version = "$([VSTeamVersions]::Release)"
-            }
-        }
-        
-        Switch($resourceType)
-        {
-            "Repository"
-            {
-                $resp = (Get-VSTeamAccessControlList -SecurityNamespaceId $securityNamespaceID -token $token -ErrorAction SilentlyContinue | Select-Object -ExpandProperty InheritPermissions -ErrorAction SilentlyContinue )
-            }
-
-            {($resourceType -eq "BuildDefinition") -or ($resourceType -eq "ReleaseDefinition")}
-            {
-                $body = @"
+      if ($resourceType -eq "Repository") {
+         $resp = (Get-VSTeamAccessControlList -SecurityNamespaceId $securityNamespaceID -token $token | Select-Object -ExpandProperty InheritPermissions)
+      }
+      else {
+         $body = @"
 {
     "contributionIds":["ms.vss-admin-web.security-view-data-provider"],
     "dataProviderContext":
@@ -104,22 +46,23 @@
     }
 }
 "@
-                $resp = (_callAPI -method POST -area "Contribution" -resource "HierarchyQuery/project" -id $projectID -Version "$Version" -ContentType "application/json" -Body $body -ErrorAction SilentlyContinue | Select-Object -ExpandProperty dataProviders -ErrorAction SilentlyContinue | Select-Object -ExpandProperty 'ms.vss-admin-web.security-view-data-provider' -ErrorAction SilentlyContinue | Select-Object -ExpandProperty permissionsContextJson -ErrorAction SilentlyContinue | ConvertFrom-Json | Select-Object -ExpandProperty inheritPermissions -ErrorAction SilentlyContinue)
-            } 
-        }
+         $resp = (_callAPI -method POST -area "Contribution" -resource "HierarchyQuery/project" -id $projectID -Version $version -ContentType "application/json" -Body $body |
+            Select-Object -ExpandProperty dataProviders |
+            Select-Object -ExpandProperty 'ms.vss-admin-web.security-view-data-provider' |
+            Select-Object -ExpandProperty permissionsContextJson |
+            ConvertFrom-Json |
+            Select-Object -ExpandProperty inheritPermissions)
+      }
 
-        Switch($resp)
-        {
-            {($resp -eq $true) -or ($resp -eq $false)}
-            {
-                return $resp
-            }
+      Switch ($resp) {
+         { ($resp -eq $true) -or ($resp -eq $false) } {
+            return $resp
+         }
 
-            {($resp -ne $true) -and ($resp -ne $false)}
-            {
-                Write-Error "Unable to retrieve permission inheritance state."
-                return
-            }
-        }
-    }
+         { ($resp -ne $true) -and ($resp -ne $false) } {
+            Write-Error "Unable to retrieve permission inheritance state."
+            return
+         }
+      }
+   }
 }
--- Source/Public/Set-VSTeamPermissionInheritance.ps1
+++ Source/Public/Set-VSTeamPermissionInheritance.ps1
@@ -1,99 +1,42 @@
 ﻿function Set-VSTeamPermissionInheritance {
-    [CmdletBinding(SupportsShouldProcess=$true, ConfirmImpact = 'High')]
-    [OutputType([System.String])]
-    param(
-       [Parameter(Mandatory)]
-       [string] $resourceName,
-       [Parameter(Mandatory)]  
-       [ValidateSet('Repository','BuildDefinition','ReleaseDefinition')]
-       [string] $resourceType,
-       [Parameter(Mandatory)]  
-       [ValidateSet('true', 'false')]
-       [string] $newState
-    )
- 
-    process 
-    {       
-        if($env:TEAM_PROJECT)
-        {
-            $projectID = (Get-VSTeamProject -Name $env:TEAM_PROJECT -ErrorAction SilentlyContinue | Select-Object -ExpandProperty id -ErrorAction SilentlyContinue)
-        }
-        else
-        {
-            Write-Error "Unable to retrieve project information. Ensure that Set-VSTeamDefaultProject has been run prior to execution."
-            Return
-        }
-        
-        Switch($resourceType)
-        {
-            "Repository"
-            {
-                $securityNamespaceID = "2e9eb7ed-3c0a-47d4-87c1-0ffdd275fd87"
+   [CmdletBinding(SupportsShouldProcess = $true, ConfirmImpact = 'High')]
+   [OutputType([System.String])]
+   param(
+      [Parameter(Mandatory)]
+      [string] $resourceName,
 
-                $repositoryID = (Get-VSTeamGitRepository -Name "$resourceName" -ProjectName "$env:TEAM_PROJECT" -WarningAction SilentlyContinue -ErrorAction SilentlyContinue | Select-Object -ExpandProperty id -ErrorAction SilentlyContinue)
+      [Parameter(Mandatory)]
+      [ValidateSet('Repository', 'BuildDefinition', 'ReleaseDefinition')]
+      [string] $resourceType,
 
-                If($null -eq $repositoryID)
-                {
-                    Write-Error "Unable to retrieve repository information. Ensure that the resourceName provided matches a repository name exactly."
-                    Return
-                }
+      [Parameter(Mandatory)]
+      [bool] $newState,
 
-                $token = "repoV2/$projectID/$repositoryID"
+      # Forces the command without confirmation
+      [switch] $Force
+   )
 
-                $Version = "$([VSTeamVersions]::Git)"
-            }
-            "BuildDefinition"
-            {
-                $securityNamespaceID = "33344d9c-fc72-4d6f-aba5-fa317101a7e9"
+   DynamicParam {
+      _buildProjectNameDynamicParam -mandatory $true
+   }
 
-                $buildDefinitionID = (Get-VSTeamBuildDefinition -ProjectName $env:team_project -ErrorAction SilentlyContinue | Where-Object name -eq "$resourceName" -ErrorAction SilentlyContinue | Select-Object -ExpandProperty id -ErrorAction SilentlyContinue)
+   process {
+      # Bind the parameter to a friendly variable
+      $ProjectName = $PSBoundParameters["ProjectName"]
+      Write-Verbose "Creating VSTeamPermissionInheritance"
+      $item = [VSTeamPermissionInheritance]::new($ProjectName, $resourceName, $resourceType)
+      $token = $item.Token
+      $version = $item.Version
+      $projectID = $item.ProjectID
+      $securityNamespaceID = $item.SecurityNamespaceID
 
-                If($null -eq $buildDefinitionID)
-                {
-                    Write-Error "Unable to retrieve build definition information. Ensure that the resourceName provided matches a build definition name exactly."
-                    Return
-                }
+      Write-Verbose "Token = $token"
+      Write-Verbose "Version = $Version"
+      Write-Verbose "ProjectID = $ProjectID"
+      Write-Verbose "SecurityNamespaceID = $SecurityNamespaceID"
 
-                $token = "$projectID/$buildDefinitionID"
-
-                $Version = "$([VSTeamVersions]::Build)"
-            }
-
-            "ReleaseDefinition"
-            {
-                $securityNamespaceID = "c788c23e-1b46-4162-8f5e-d7585343b5de"
-
-                $releaseDefinition = (Get-VSTeamReleaseDefinition -ProjectName $env:team_project -ErrorAction SilentlyContinue | Where-Object -Property name -eq "$resourceName" -ErrorAction SilentlyContinue)
-
-                If($null -eq $releaseDefinition)
-                {
-                    Write-Error "Unable to retrieve release definition information. Ensure that the resourceName provided matches a release definition name exactly."
-                    Return
-                }
-
-                if(($releaseDefinition).path -eq "/")
-                {
-                    $token = "$projectID/$($releaseDefinition.id)"
-                }
-                else
-                {
-                    $token = "$projectID"+"$($releaseDefinition.path -replace "\\","/")"+"/$($releaseDefinition.id)"
-                }
-
-                $Version = "$([VSTeamVersions]::Release)"
-            }
-        }
-        
-        #Get current inheritance state for resource to see if resource is already in desired state
-        $inheritanceCheck = (Get-VSTeamPermissionInheritance -resourceName "$resourceName" -resourceType "$resourceType")
-        
-        if((($newState -eq "$true") -and ($inheritanceCheck -eq $true)) -or (($newState -eq "$false") -and ($inheritanceCheck -eq $false)))
-        {
-            Return "Inheritance already set to $newState. Not executing"
-        }
-
-        if($PSCmdlet.ShouldProcess("$resourceName")) {
-            $body = @"
+      if ($force -or $PSCmdlet.ShouldProcess($resourceName, "Set Permission Inheritance")) {
+         $body = @"
 {
     "contributionIds":["ms.vss-admin-web.security-view-update-data-provider"],
     "dataProviderContext":
@@ -108,18 +51,16 @@
     }
 }
 "@
-            # Call the REST API to change the inheritance state
-            $resp = _callAPI -method POST -area "Contribution" -resource "HierarchyQuery" -id $projectID -Version "$Version" -ContentType "application/json" -Body $body -WarningAction SilentlyContinue -ErrorAction SilentlyContinue
-        }
-          
-        if(($resp | Select-Object -ExpandProperty dataProviders -ErrorAction SilentlyContinue | Select-Object -ExpandProperty 'ms.vss-admin-web.security-view-update-data-provider' -ErrorAction SilentlyContinue | Select-Object -ExpandProperty statusCode -ErrorAction SilentlyContinue) -eq "204")
-        {   
-            Return "Inheritance successfully changed for $resourceType $resourceName."
-        }
-        else
-        {           
-            Write-Error "Inheritance change failed for $resourceType $resourceName."
-            Return
-        }
-    }
+         # Call the REST API to change the inheritance state
+         $resp = _callAPI -method POST -area "Contribution" -resource "HierarchyQuery" -id $projectID -Version $version -ContentType "application/json" -Body $body
+      }
+
+      if (($resp | Select-Object -ExpandProperty dataProviders | Select-Object -ExpandProperty 'ms.vss-admin-web.security-view-update-data-provider' | Select-Object -ExpandProperty statusCode) -eq "204") {
+         Return "Inheritance successfully changed for $resourceType $resourceName."
+      }
+      else {
+         Write-Error "Inheritance change failed for $resourceType $resourceName."
+         Return
+      }
+   }
 }
