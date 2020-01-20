95860f688e4c6bda22e2b227a30e0fce8c088be3
James O'Neill <P10506111@capita.co.uk>
2020-01-20 17:45:36 +0000

    import classes via PSD1, add exportFunctions list

--- Source/VSTeam.psm1
+++ Source/VSTeam.psm1
@@ -1,15 +1,4 @@
-# Dot sources all the files in Public, Private and Classes
-# Then reads the file names in Public and exports them
-# as the fucntions for the module.
-# To export an alias you have to manually add it to the
-# Export-ModuleMember below.
-# The module manifest is using wildcard exports for functions
-# and alias so you only have to name the files correctly.
-
-# Files are built using a script in the root folder
-. "$PSScriptRoot\vsteam.classes.ps1"
-. "$PSScriptRoot\vsteam.functions.ps1"
-
+. $PSScriptRoot\vsteam.functions.ps1 
 # Check to see if the user stored the default project in an environment variable
 if ($null -ne $env:TEAM_PROJECT) {
    # Make sure the value in the environment variable still exisits.
@@ -23,3 +12,6 @@ if ($null -ne $env:TEAM_PROJECT) {
 
 # Load the correct version of the environment variable
 Set-VSTeamAPIVersion -Target $([VSTeamVersions]::Version)
+
+
+get-command _getProcesses,_getProjects,_getWorkItemTypes  | ForEach-Object { $_.Visibility =  [System.Management.Automation.SessionStateEntryVisibility]::Private}
