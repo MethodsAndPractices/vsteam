5892b143bf0e98c2dd1214fce78a71a9586f10ac
Donovan Brown <dbrown@microsoft.com>
2020-03-13 11:22:20 -0500

    Changed mock to return object instead of hashtable.

--- unit/test/Invoke-VSTeamRequest.Tests.ps1
+++ unit/test/Invoke-VSTeamRequest.Tests.ps1
@@ -10,7 +10,9 @@ $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".")
 
 Describe 'Invoke-VSTeamRequest' {
    # Mock the call to Get-Projects by the dynamic parameter for ProjectName
-   Mock Invoke-RestMethod { return @() } -ParameterFilter {
+   Mock Invoke-RestMethod { 
+      return @()
+   } -ParameterFilter {
       $Uri -like "*_apis/projects*"
    }
 
@@ -48,7 +50,7 @@ Describe 'Invoke-VSTeamRequest' {
          $Headers["Test"] -eq 'Test'
       }
 
-      Invoke-VSTeamRequest -Area release -Resource releases -Id 1 -SubDomain vsrm -Version '4.1-preview' -ProjectName testproject -JSON -AdditionalHeaders @{Test = "Test"}
+      Invoke-VSTeamRequest -Area release -Resource releases -Id 1 -SubDomain vsrm -Version '4.1-preview' -ProjectName testproject -JSON -AdditionalHeaders @{Test = "Test" }
 
       It 'Should call API' {
          Assert-VerifiableMock
@@ -56,11 +58,12 @@ Describe 'Invoke-VSTeamRequest' {
    }
 
    Context 'Invoke-VSTeamRequest By Product ID' {
-      Mock _getInstance { return 'https://dev.azure.com/test' } -Verifiable
       # Called to convert from ProjectName to ProjectID
-      Mock Get-VSTeamProject { return @{
-         ID = '00000000-0000-0000-0000-000000000000'
-      }};
+      Mock Get-VSTeamProject {
+         return [PSCustomObject]@{
+            id = '00000000-0000-0000-0000-000000000000'
+         } } -Verifiable
+      Mock _getInstance { return 'https://dev.azure.com/test' } -Verifiable
       Mock Invoke-RestMethod { return @() } -Verifiable -ParameterFilter {
          $Uri -like "*https://vsrm.dev.azure.com/test/00000000-0000-0000-0000-000000000000*"
       }
