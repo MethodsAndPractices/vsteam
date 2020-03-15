4beecec71603035ce4ee262988d03416c5166a47
Donovan Brown <dbrown@microsoft.com>
2020-03-15 17:48:26 -0500

    Removed InModuleScope from Add-VSTeamPullRequest

--- unit/test/Add-VSTeamPullRequest.Tests.ps1
+++ unit/test/Add-VSTeamPullRequest.Tests.ps1
@@ -1,70 +1,75 @@
 Set-StrictMode -Version Latest
 
-InModuleScope VSTeam {
+$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".")
 
-    Describe 'Pull Requests' {
-        . "$PSScriptRoot\mocks\mockProjectNameDynamicParamNoPSet.ps1"
+. "$here/../../Source/Classes/VSTeamVersions.ps1"
+. "$here/../../Source/Private/common.ps1"
+. "$here/../../Source/Private/applyTypes.ps1"
+. "$here/../../Source/Public/$sut"
 
-        Mock _getInstance { return 'https://dev.azure.com/test' } -Verifiable
+Describe 'Add-VSTeamPullRequest' {
+   . "$PSScriptRoot\mocks\mockProjectNameDynamicParamNoPSet.ps1"
 
-        # You have to set the version or the api-version will not be added when
-        # [VSTeamVersions]::Core = ''
-        [VSTeamVersions]::Git = '5.1-preview'
-        [VSTeamVersions]::Graph = '5.0'
+   Mock _getInstance { return 'https://dev.azure.com/test' } -Verifiable
 
-       $result = Get-Content "$PSScriptRoot\sampleFiles\updatePullRequestResponse.json" -Raw | ConvertFrom-Json
+   # You have to set the version or the api-version will not be added when
+   # [VSTeamVersions]::Core = ''
+   [VSTeamVersions]::Git = '5.1-preview'
+   [VSTeamVersions]::Graph = '5.0'
 
-       Context 'Add-VSTeamPullRequest' {
+   $result = Get-Content "$PSScriptRoot\sampleFiles\updatePullRequestResponse.json" -Raw | ConvertFrom-Json
 
-            It 'Add-VSTeamPullRequest as Draft' {
-                Mock Invoke-RestMethod { return $result }
+   Context 'Add-VSTeamPullRequest' {
 
-                Add-VSTeamPullRequest -RepositoryId "45df2d67-e709-4557-a7f9-c6812b449277" -ProjectName "Sandbox" `
-                  -Title "PR Title" -Description "PR Description" `
-                  -SourceRefName "refs/heads/test" -TargetRefName "refs/heads/master" `
-                  -Draft -Force
+      It 'Add-VSTeamPullRequest as Draft' {
+         Mock Invoke-RestMethod { return $result }
 
-                Assert-MockCalled Invoke-RestMethod -Scope It -ParameterFilter {
-                    $Method -eq 'Post' -and
-                    $Uri -like "*repositories/45df2d67-e709-4557-a7f9-c6812b449277/*" -and
-                    $Uri -like "*pullrequests*" -and
-                    $Body -eq '{"sourceRefName": "refs/heads/test", "targetRefName": "refs/heads/master", "title": "PR Title", "description": "PR Description", "isDraft": true}'
-                }
-            }
+         Add-VSTeamPullRequest -RepositoryId "45df2d67-e709-4557-a7f9-c6812b449277" -ProjectName "Sandbox" `
+            -Title "PR Title" -Description "PR Description" `
+            -SourceRefName "refs/heads/test" -TargetRefName "refs/heads/master" `
+            -Draft -Force
 
-            It 'Add-VSTeamPullRequest as Published' {
-               Mock Invoke-RestMethod { return $result }
+         Assert-MockCalled Invoke-RestMethod -Scope It -ParameterFilter {
+            $Method -eq 'Post' -and
+            $Uri -like "*repositories/45df2d67-e709-4557-a7f9-c6812b449277/*" -and
+            $Uri -like "*pullrequests*" -and
+            $Body -eq '{"sourceRefName": "refs/heads/test", "targetRefName": "refs/heads/master", "title": "PR Title", "description": "PR Description", "isDraft": true}'
+         }
+      }
 
-               Add-VSTeamPullRequest -RepositoryId "45df2d67-e709-4557-a7f9-c6812b449277" -ProjectName "Sandbox" `
-                 -Title "PR Title" -Description "PR Description" `
-                 -SourceRefName "refs/heads/test" -TargetRefName "refs/heads/master" `
-                 -Force
+      It 'Add-VSTeamPullRequest as Published' {
+         Mock Invoke-RestMethod { return $result }
 
-               Assert-MockCalled Invoke-RestMethod -Scope It -ParameterFilter {
-                   $Method -eq 'Post' -and
-                   $Uri -like "*repositories/45df2d67-e709-4557-a7f9-c6812b449277/*" -and
-                   $Uri -like "*pullrequests*" -and
-                   $Body -eq '{"sourceRefName": "refs/heads/test", "targetRefName": "refs/heads/master", "title": "PR Title", "description": "PR Description", "isDraft": false}'
-               }
-           }
+         Add-VSTeamPullRequest -RepositoryId "45df2d67-e709-4557-a7f9-c6812b449277" -ProjectName "Sandbox" `
+            -Title "PR Title" -Description "PR Description" `
+            -SourceRefName "refs/heads/test" -TargetRefName "refs/heads/master" `
+            -Force
 
-            It 'Add-VSTeamPullRequest with wrong -SourceRefName throws' {
-               { 
-                  Add-VSTeamPullRequest -RepositoryId "45df2d67-e709-4557-a7f9-c6812b449277" -ProjectName "Sandbox" `
-                  -Title "PR Title" -Description "PR Description" `
-                  -SourceRefName "garbage" -TargetRefName "refs/heads/master" `
-                  -Draft -Force 
-               } | Should Throw
-            }
+         Assert-MockCalled Invoke-RestMethod -Scope It -ParameterFilter {
+            $Method -eq 'Post' -and
+            $Uri -like "*repositories/45df2d67-e709-4557-a7f9-c6812b449277/*" -and
+            $Uri -like "*pullrequests*" -and
+            $Body -eq '{"sourceRefName": "refs/heads/test", "targetRefName": "refs/heads/master", "title": "PR Title", "description": "PR Description", "isDraft": false}'
+         }
+      }
 
-            It 'Add-VSTeamPullRequest with wrong -TargetRefName throws' {
-               { 
-                  Add-VSTeamPullRequest -RepositoryId "45df2d67-e709-4557-a7f9-c6812b449277" -ProjectName "Sandbox" `
-                  -Title "PR Title" -Description "PR Description" `
-                  -SourceRefName "refs/heads/test" -TargetRefName "garbage" `
-                  -Draft -Force 
-               } | Should Throw
-            }
-        }
-    }
+      It 'Add-VSTeamPullRequest with wrong -SourceRefName throws' {
+         { 
+            Add-VSTeamPullRequest -RepositoryId "45df2d67-e709-4557-a7f9-c6812b449277" -ProjectName "Sandbox" `
+               -Title "PR Title" -Description "PR Description" `
+               -SourceRefName "garbage" -TargetRefName "refs/heads/master" `
+               -Draft -Force 
+         } | Should Throw
+      }
+
+      It 'Add-VSTeamPullRequest with wrong -TargetRefName throws' {
+         { 
+            Add-VSTeamPullRequest -RepositoryId "45df2d67-e709-4557-a7f9-c6812b449277" -ProjectName "Sandbox" `
+               -Title "PR Title" -Description "PR Description" `
+               -SourceRefName "refs/heads/test" -TargetRefName "garbage" `
+               -Draft -Force 
+         } | Should Throw
+      }
+   }
 }
