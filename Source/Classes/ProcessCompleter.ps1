9d53c008794ecd956f36b567d78ec294e7a4f96b
James O'Neill <P10506111@capita.co.uk>
2020-03-17 21:25:21 +0000

    fix ProcessCompleter

--- Source/Classes/ProcessCompleter.ps1
+++ Source/Classes/ProcessCompleter.ps1
@@ -1,12 +1,16 @@
-class sCompleter : System.Management.Automation.IArgumentCompleter {
+class ProcessCompleter : System.Management.Automation.IArgumentCompleter {
   [System.Collections.Generic.IEnumerable[System.Management.Automation.CompletionResult]] CompleteArgument(
         [string]$CommandName, [string]$ParameterName, [string]$WordToComplete,
         [System.Management.Automation.Language.CommandAst]$CommandAst, [System.Collections.IDictionary] $FakeBoundParameters
   )    {
-        [VSTeamsCache]::ses  =  _getses
-        [VSTeamsCache]::timestamp = (get-date).TimeOfDay.TotalMinutes
+        $minutesNow = (Get-Date).TimeOfDay.TotalMinutes
+        if ( (([VSTeamProcessCache]::processes -notcontains $arguments) -or 
+              ([VSTeamProcessCache]::timestamp + 100 -lt $minutesNow) ) -and ( [VSTeamVersions]::Account) ) { 
+               [VSTeamProcessCache]::processes = (Invoke-VSTeamRequest -url  ('{0}/_apis/process/processes?api-version={1}&stateFilter=All&$top=9999' -f [VSTeamVersions]::Account, [VSTeamVersions]::Core  )).value.name
+               [VSTeamProcessCache]::timestamp = $minutesNow
+        }
         $results = [System.Collections.Generic.List[System.Management.Automation.CompletionResult]]::new()
-        foreach ($p in [VSTeamsCache]::ses ) {
+        foreach ($p in [VSTeamProcessCache]::processes ) {
             if ($p -like "*$WordToComplete*") {
                 $results.Add([System.Management.Automation.CompletionResult]::new($p))
             }
