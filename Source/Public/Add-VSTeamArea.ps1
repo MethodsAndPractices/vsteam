ced9f0ce926abd356a28e8250335d75af2b201a4
Sebastian Schütze <sebastian.schuetze@razorspoint.com>
2020-04-19 18:42:38 +0200

    added Add-VsTeamArea

---
+++ Source/Public/Add-VSTeamArea.ps1
@@ -0,0 +1,25 @@
+function Add-VSTeamClassificationNode {
+   [CmdletBinding()]
+   param(
+      [Parameter(Mandatory = $true)]
+      [string] $Name,
+
+      [Parameter(Mandatory = $false)]
+      [string] $Path
+   )
+
+   DynamicParam {
+      _buildProjectNameDynamicParam -Mandatory $true
+   }
+
+   process {
+      # Bind the parameter to a friendly variable
+      $ProjectName = $PSBoundParameters["ProjectName"]
+
+      $resp = Add-VSTeamClassificationNode -Name $Name -StructureGroup "areas" -Path $Path -ProjectName $ProjectName
+
+      $resp = [VSTeamClassificationNode]::new($resp, $ProjectName)
+
+      Write-Output $resp
+   }
+}
