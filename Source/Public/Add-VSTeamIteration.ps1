b635b5192150bd0259c8be80f38c7829e36ff2d2
Sebastian Schütze <sebastian.schuetze@razorspoint.com>
2020-04-19 18:43:00 +0200

    added Add-VsTeamIteration

---
+++ Source/Public/Add-VSTeamIteration.ps1
@@ -0,0 +1,35 @@
+function Add-VSTeamClassificationNode {
+   [CmdletBinding()]
+   param(
+      [CmdletBinding(DefaultParameterSetName = 'ByIteration')]
+      [Parameter(Mandatory = $true)]
+      [string] $Name,
+
+      [CmdletBinding(DefaultParameterSetName = 'ByIteration')]      
+      [Parameter(Mandatory = $false)]
+      [string] $Path,
+
+      [CmdletBinding(DefaultParameterSetName = 'ByIteration')]      
+      [Parameter(Mandatory = $false)]
+      [datetime] $StartDate,
+
+      [CmdletBinding(DefaultParameterSetName = 'ByIteration')]      
+      [Parameter(Mandatory = $false)]
+      [datetime] $FinishDate
+   )
+
+   DynamicParam {
+      _buildProjectNameDynamicParam -Mandatory $true
+   }
+
+   process {
+      # Bind the parameter to a friendly variable
+      $ProjectName = $PSBoundParameters["ProjectName"]
+
+      $resp = Add-VSTeamClassificationNode -Name $Name -StructureGroup "iteration" -Path $Path -ProjectName $ProjectName -StartDate $StartDate -FinishDate $FinishDate
+
+      $resp = [VSTeamClassificationNode]::new($resp, $ProjectName)
+
+      Write-Output $resp
+   }
+}
