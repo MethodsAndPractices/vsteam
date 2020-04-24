393122e0f0d08cebee42f8c446f39ea6af1189c1
Sebastian Schütze <sebastian.schuetze@razorspoint.com>
2020-04-24 23:41:46 +0200

    added unit tests for Add-VSTeamClassificationNode

---
+++ unit/test/Add-VSTeamClassificationNode.Tests.ps1
@@ -0,0 +1,71 @@
+Set-StrictMode -Version Latest
+
+#region include
+Import-Module SHiPS
+
+$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".")
+
+. "$here/../../Source/Classes/VSTeamLeaf.ps1"
+. "$here/../../Source/Classes/VSTeamVersions.ps1"
+. "$here/../../Source/Classes/VSTeamProjectCache.ps1"
+. "$here/../../Source/Classes/VSTeamClassificationNode.ps1"
+. "$here/../../Source/Private/common.ps1"
+. "$here/../../Source/Public/$sut"
+#endregion
+
+Describe 'Add-VSTeamClassificationNode' {
+
+   $classificationNodeResult = Get-Content "$PSScriptRoot\sampleFiles\classificationNodeResult.json" -Raw | ConvertFrom-Json
+
+
+   Mock _getInstance { return 'https://dev.azure.com/test' }
+   Mock _getApiVersion { return '5.0-unitTests' } -ParameterFilter { $Service -eq 'Core' }
+
+   # Mock the call to Get-Projects by the dynamic parameter for ProjectName
+   Mock Invoke-RestMethod { return @() } -ParameterFilter { $Uri -like "*_apis/projects*" }
+
+   Context 'simplest call' {
+      Mock Invoke-RestMethod { 
+         Write-Host $args
+
+         return $classificationNodeResult 
+      }
+      
+
+      It 'with StructureGroup "<StructureGroup>" should return Nodes' -TestCases @(
+         @{StructureGroup = "areas" }
+         @{StructureGroup = "iterations" }
+     ) {
+         param ($StructureGroup)      
+         ## Act
+         Add-VSTeamClassificationNode -ProjectName "Public Demo" -StructureGroup $StructureGroup -Name "MyClassificationNodeName"
+
+         ## Assert
+         Assert-MockCalled Invoke-RestMethod -Exactly -Times 1 -Scope It -ParameterFilter {
+            $Uri -like "https://dev.azure.com/test/Public Demo/_apis/wit/classificationnodes/$StructureGroup*" -and
+            $Uri -like "*api-version=$(_getApiVersion Core)*" -and
+            $Body -like '*"name":*"MyClassificationNodeName"*'
+         }
+      }    
+
+      It 'with StructureGroup "<StructureGroup>" by Path <Path> should return Nodes' -TestCases @(
+         @{StructureGroup = "areas"; Path = "\" }
+         @{StructureGroup = "areas"; Path = "\SubPath\" }
+         @{StructureGroup = "iterations"; Path = "\" }
+         @{StructureGroup = "iterations"; Path = "\SubPath\" }
+     ) {
+         param ($StructureGroup, $Path)      
+         ## Act
+         Add-VSTeamClassificationNode -ProjectName "Public Demo" -StructureGroup $StructureGroup -Name "MyClassificationNodeName" -Path $Path
+
+         ## Assert
+         Assert-MockCalled Invoke-RestMethod -Exactly -Times 1 -Scope It -ParameterFilter {
+            $Uri -like "https://dev.azure.com/test/Public Demo/_apis/wit/classificationnodes/$StructureGroup*" #-and
+            $Uri -like "*api-version=$(_getApiVersion Core)*" -and
+            $Body -like '*"name":*"MyClassificationNodeName"*'
+            $Body -like "*""path"":*""$Path""*"
+         }
+      }
+   }
+}
