97b80a920b99d38908a24e9e95b5f655c634a5c0
Sebastian Schütze <sebastian.schuetze@razorspoint.com>
2020-04-27 17:24:29 +0200

    added unit test for Remove-VSTeamArea.Tests

---
+++ unit/test/Remove-VSTeamArea.Tests.ps1
@@ -0,0 +1,88 @@
+Set-StrictMode -Version Latest
+
+#region include
+Import-Module SHiPS
+
+$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".")
+
+. "$here/../../Source/Classes/VSTeamLeaf.ps1"
+. "$here/../../Source/Classes/VSTeamVersions.ps1"
+. "$here/../../Source/Classes/VSTeamProjectCache.ps1"
+. "$here/../../Source/Classes/VSTeamClassificationNode.ps1"
+. "$here/../../Source/Private/common.ps1"
+. "$here/../../Source/Public/Remove-VSTeamClassificationNode.ps1"
+. "$here/../../Source/Public/$sut"
+#endregion
+
+Describe 'Remove-VSTeamArea' {
+
+   Mock _getInstance { return 'https://dev.azure.com/test' }
+   Mock _getApiVersion { return '5.0-unitTests' } -ParameterFilter { $Service -eq 'Core' }
+
+   Context 'simplest call' {
+      Mock Invoke-RestMethod {
+         #Write-Host $args
+         return $null 
+      }      
+
+      It 'should delete area' -TestCases @(
+      ) {     
+         ## Act
+         Remove-VSTeamArea -ProjectName "Public Demo"
+         ## Assert
+         Assert-MockCalled Invoke-RestMethod -Exactly -Times 1 -Scope It -ParameterFilter {
+            $Method -eq "Delete" -and
+            $Uri -like "https://dev.azure.com/test/Public Demo/_apis/wit/classificationnodes/areas*" -and
+            $Uri -like "*api-version=$(_getApiVersion Core)*"
+         }
+      }
+
+      It 'should delete area with reclassification id <ReClassifyId>' -TestCases @(
+         @{ReClassifyId = 4}
+      ) {
+         param ($ReClassifyId)      
+         ## Act
+         Remove-VSTeamArea -ProjectName "Public Demo" -ReClassifyId $ReClassifyId
+         ## Assert
+         Assert-MockCalled Invoke-RestMethod -Exactly -Times 1 -Scope It -ParameterFilter {
+            $Method -eq "Delete" -and
+            $Uri -like "https://dev.azure.com/test/Public Demo/_apis/wit/classificationnodes/areas*" -and
+            $Uri -like "*api-version=$(_getApiVersion Core)*"
+         }
+      }
+
+      It 'with Path "<Path>" should delete area' -TestCases @(
+         @{Path = "SubPath" }
+         @{Path = "Path/SubPath" }
+         @{Path = "Path\SubPath" }
+      ) {
+         param ($Path)      
+         ## Act
+         Remove-VSTeamArea -ProjectName "Public Demo" -Path $Path
+
+         ## Assert
+         Assert-MockCalled Invoke-RestMethod -Exactly -Times 1 -Scope It -ParameterFilter {
+            $Method -eq "Delete" -and
+            $Uri -like "https://dev.azure.com/test/Public Demo/_apis/wit/classificationnodes/areas/$Path*" -and
+            $Uri -like "*api-version=$(_getApiVersion Core)*"
+         }
+      }
+      
+      It 'with empty Path "<Path>" should delete area' -TestCases @(
+         @{Path = "" }
+         @{Path = $null }
+      ) {
+         param ($Path)      
+         ## Act
+         Remove-VSTeamArea -ProjectName "Public Demo" -Path $Path
+
+         ## Assert
+         Assert-MockCalled Invoke-RestMethod -Exactly -Times 1 -Scope It -ParameterFilter {
+            $Method -eq "Delete" -and
+            $Uri -like "https://dev.azure.com/test/Public Demo/_apis/wit/classificationnodes/areas?*" -and
+            $Uri -like "*api-version=$(_getApiVersion Core)*"
+         }
+      }
+   }
+}
