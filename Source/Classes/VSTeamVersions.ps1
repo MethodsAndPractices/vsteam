3ea84ce1d3b9ccc77ca935bdc96c77b31e0e4cd2
Sebastian Schütze <sebastian.schuetze@razorspoint.com>
2020-05-12 17:56:43 +0200

    seting default Build API version from 3.0 to 3.2 according to #78

--- Source/Classes/VSTeamVersions.ps1
+++ Source/Classes/VSTeamVersions.ps1
@@ -2,7 +2,7 @@ class VSTeamVersions {
    static [string] $Account = $env:TEAM_ACCT
    static [string] $DefaultProject = $env:TEAM_PROJECT
    static [string] $Version = $(If ($env:TEAM_VERSION) { $env:TEAM_VERSION } Else { "TFS2017" })
-   static [string] $Build = '3.0'
+   static [string] $Build = '3.2'
    static [string] $Release = '3.0-preview'
    static [string] $Core = '3.0'
    static [string] $Git = '3.0'
