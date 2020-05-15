ef47d1546631ae15de307a97f1b50b485255593f
Donovan Brown <dbrown@microsoft.com>
2020-05-15 13:58:01 -0500

    Get-VSTeamResourceArea

--- unit/test/Get-VSTeamResourceArea.Tests.ps1
+++ unit/test/Get-VSTeamResourceArea.Tests.ps1
@@ -1,27 +1,28 @@
 Set-StrictMode -Version Latest
 
-#region include
-$here = Split-Path -Parent $MyInvocation.MyCommand.Path
-$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".")
-
-. "$here/../../Source/Classes/VSTeamVersions.ps1"
-. "$here/../../Source/Classes/VSTeamProjectCache.ps1"
-. "$here/../../Source/Private/common.ps1"
-. "$here/../../Source/Private/applyTypes.ps1"
-. "$here/../../Source/Public/$sut"
-#endregion
-
 Describe 'VSTeamResourceArea' {
+   BeforeAll {
+      $sut = (Split-Path -Leaf $PSCommandPath).Replace(".Tests.", ".")
+
+      . "$PSScriptRoot/../../Source/Classes/VSTeamVersions.ps1"
+      . "$PSScriptRoot/../../Source/Classes/VSTeamProjectCache.ps1"
+      . "$PSScriptRoot/../../Source/Private/common.ps1"
+      . "$PSScriptRoot/../../Source/Private/applyTypes.ps1"
+      . "$PSScriptRoot/../../Source/Public/$sut"
+   }
+
    Context 'Get-VSTeamResourceArea' {
-      ## Arrange
-      Mock _callAPI { return @{ value = @{ } } }
+      BeforeAll {
+         ## Arrange
+         Mock _callAPI { return @{ value = @{ } } }
+      }
 
       It 'Should return resources' {
          ## Act
          $actual = Get-VSTeamResourceArea
 
          ## Assert
-         $actual | Should Not Be $null
+         $actual | Should -Not -Be $null
       }
    }
 }
