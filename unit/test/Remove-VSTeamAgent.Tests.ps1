745275520231e9c9b97bad99255aafdb7bd72395
Donovan Brown <dbrown@microsoft.com>
2020-05-15 15:23:02 -0500

    Remove-VSTeamAgent

--- unit/test/Remove-VSTeamAgent.Tests.ps1
+++ unit/test/Remove-VSTeamAgent.Tests.ps1
@@ -1,32 +1,33 @@
 Set-StrictMode -Version Latest
 
-#region include
-$here = Split-Path -Parent $MyInvocation.MyCommand.Path
-$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".")
-
-. "$here/../../Source/Classes/VSTeamVersions.ps1"
-. "$here/../../Source/Classes/VSTeamProjectCache.ps1"
-. "$here/../../Source/Classes/ProjectValidateAttribute.ps1"
-. "$here/../../Source/Private/common.ps1"
-. "$here/../../Source/Public/$sut"
-#endregion
-
-Describe 'Remove-VSTeamAgent' {
-   Mock _getInstance { return 'https://dev.azure.com/test' } -Verifiable
-   [VSTeamVersions]::DistributedTask = '1.0-unitTest'
+Describe 'VSTeamAgent' {
+   BeforeAll {
+      $sut = (Split-Path -Leaf $PSCommandPath).Replace(".Tests.", ".")
    
-   # Mock the call to Get-Projects by the dynamic parameter for ProjectName
-   Mock Invoke-RestMethod { return @() } -ParameterFilter {
-      $Uri -like "*_apis/projects*"
+      . "$PSScriptRoot/../../Source/Classes/VSTeamVersions.ps1"
+      . "$PSScriptRoot/../../Source/Classes/VSTeamProjectCache.ps1"
+      . "$PSScriptRoot/../../Source/Classes/ProjectValidateAttribute.ps1"
+      . "$PSScriptRoot/../../Source/Private/common.ps1"
+      . "$PSScriptRoot/../../Source/Public/$sut"
+   
+      Mock _getInstance { return 'https://dev.azure.com/test' } -Verifiable
+      [VSTeamVersions]::DistributedTask = '1.0-unitTest'
+
+      # Mock the call to Get-Projects by the dynamic parameter for ProjectName
+      Mock Invoke-RestMethod { return @() } -ParameterFilter {
+         $Uri -like "*_apis/projects*"
+      }
    }
 
    Context 'Remove-VSTeamAgent by ID' {
-      Mock Invoke-RestMethod
+      BeforeAll {
+         Mock Invoke-RestMethod
+      }
 
       It 'should remove the agent with passed in Id' {
          Remove-VSTeamAgent -Pool 36 -Id 950 -Force
 
-         Assert-MockCalled Invoke-RestMethod -Exactly -Scope It -Times 1 -ParameterFilter {
+         Should -Invoke Invoke-RestMethod -Exactly -Scope It -Times 1 -ParameterFilter {
             $Method -eq 'Delete' -and
             $Uri -eq "https://dev.azure.com/test/_apis/distributedtask/pools/36/agents/950?api-version=$(_getApiVersion DistributedTask)"
          }
@@ -34,10 +35,13 @@ Describe 'Remove-VSTeamAgent' {
    }
 
    Context 'Remove-VSTeamAgent throws' {
-      Mock Invoke-RestMethod { throw 'boom' }
+      BeforeAll {
+         Mock Invoke-RestMethod { throw 'boom' }
+      }
 
       It 'should remove the agent with passed in Id' {
-         { Remove-VSTeamAgent -Pool 36 -Id 950 -Force } | Should Throw
+         { Remove-VSTeamAgent -Pool 36 -Id 950 -Force } | Should -Throw
       }
    }
 }
+
