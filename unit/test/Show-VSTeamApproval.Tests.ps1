79df8408b6c1fd8c39988d614125f166047153de
Donovan Brown <dbrown@microsoft.com>
2020-05-16 08:44:36 -0500

    Show-VSTeamApproval

--- unit/test/Show-VSTeamApproval.Tests.ps1
+++ unit/test/Show-VSTeamApproval.Tests.ps1
@@ -1,34 +1,36 @@
 Set-StrictMode -Version Latest
 
-#region include
-$here = Split-Path -Parent $MyInvocation.MyCommand.Path
-$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".")
-
-. "$here/../../Source/Classes/VSTeamVersions.ps1"
-. "$here/../../Source/Classes/VSTeamProjectCache.ps1"
-. "$here/../../Source/Classes/ProjectCompleter.ps1"
-. "$here/../../Source/Classes/ProjectValidateAttribute.ps1"
-. "$here/../../Source/Private/common.ps1"
-. "$here/../../Source/Public/$sut"
-#endregion
-
 Describe 'Show-VSTeamApproval' -Tag 'unit', 'approvals' {
-   # Set the account to use for testing. A normal user would do this
-   # using the Set-VSTeamAccount function.
-   Mock _getInstance { return 'https://dev.azure.com/test' }
-   
-   # Mock the call to Get-Projects by the dynamic parameter for ProjectName
-   Mock Invoke-RestMethod { return @() } -ParameterFilter {
-      $Uri -like "*_apis/projects*"
+   BeforeAll {
+      $sut = (Split-Path -Leaf $PSCommandPath).Replace(".Tests.", ".")
+      
+      . "$PSScriptRoot/../../Source/Classes/VSTeamVersions.ps1"
+      . "$PSScriptRoot/../../Source/Classes/VSTeamProjectCache.ps1"
+      . "$PSScriptRoot/../../Source/Classes/ProjectCompleter.ps1"
+      . "$PSScriptRoot/../../Source/Classes/ProjectValidateAttribute.ps1"
+      . "$PSScriptRoot/../../Source/Private/common.ps1"
+      . "$PSScriptRoot/../../Source/Public/$sut"
+      
+      # Set the account to use for testing. A normal user would do this
+      # using the Set-VSTeamAccount function.
+      Mock _getInstance { return 'https://dev.azure.com/test' }
+
+      # Mock the call to Get-Projects by the dynamic parameter for ProjectName
+      Mock Invoke-RestMethod { return @() } -ParameterFilter {
+         $Uri -like "*_apis/projects*"
+      }
    }
 
    Context 'Succeeds' {
-      Mock Show-Browser -Verifiable
+      BeforeAll {
+         Mock Show-Browser -Verifiable
 
-      Show-VSTeamApproval -projectName project -ReleaseDefinitionId 1
+         Show-VSTeamApproval -projectName project -ReleaseDefinitionId 1
+      }
 
       It 'should open in browser' {
-         Assert-VerifiableMock
+         Should -InvokeVerifiable
       }
    }
 }
+
