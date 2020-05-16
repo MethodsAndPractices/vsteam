3b8c7955a0e1b1e8214426e352e80b660024c298
Donovan Brown <dbrown@microsoft.com>
2020-05-16 09:05:02 -0500

    Show-VSTeamProject

--- unit/test/Show-VSTeamProject.Tests.ps1
+++ unit/test/Show-VSTeamProject.Tests.ps1
@@ -1,43 +1,43 @@
 Set-StrictMode -Version Latest
 
-#region include
-$here = Split-Path -Parent $MyInvocation.MyCommand.Path
-$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".")
-
-. "$here/../../Source/Classes/VSTeamVersions.ps1"
-. "$here/../../Source/Classes/VSTeamProjectCache.ps1"
-. "$here/../../Source/Classes/ProjectCompleter.ps1"
-. "$here/../../Source/Classes/ProjectValidateAttribute.ps1"
-. "$here/../../Source/Private/common.ps1"
-. "$here/../../Source/Public/$sut"
-#endregion
-
 Describe 'VSTeamProject' {
-   Mock _getInstance { return 'https://dev.azure.com/test' }
+   BeforeAll {
+
+      $sut = (Split-Path -Leaf $PSCommandPath).Replace(".Tests.", ".")
+
+      . "$PSScriptRoot/../../Source/Classes/VSTeamVersions.ps1"
+      . "$PSScriptRoot/../../Source/Classes/VSTeamProjectCache.ps1"
+      . "$PSScriptRoot/../../Source/Classes/ProjectCompleter.ps1"
+      . "$PSScriptRoot/../../Source/Classes/ProjectValidateAttribute.ps1"
+      . "$PSScriptRoot/../../Source/Private/common.ps1"
+      . "$PSScriptRoot/../../Source/Public/$sut"
+
+      Mock _getInstance { return 'https://dev.azure.com/test' }
+
+      Mock Show-Browser
+   }
 
    Context 'Show-VSTeamProject' {
-      Mock Show-Browser
-
       It 'by ID should call start' {
          Show-VSTeamProject -Id 123456
 
-         Assert-MockCalled Show-Browser -Exactly -Times 1 -Scope It -ParameterFilter {
+         Should -Invoke Show-Browser -Exactly -Times 1 -Scope It -ParameterFilter {
             $Url -eq "https://dev.azure.com/test/123456"
          }
       }
-   
+
       It 'by nameed ProjectName parameter should call open' {
          Show-VSTeamProject -ProjectName ShowProject
 
-         Assert-MockCalled Show-Browser -Exactly -Times 1 -Scope It -ParameterFilter {
+         Should -Invoke Show-Browser -Exactly -Times 1 -Scope It -ParameterFilter {
             $Url -eq "https://dev.azure.com/test/ShowProject"
          }
       }
-   
+
       It 'by postion projectName parameter should call open' {
          Show-VSTeamProject ShowProject
 
-         Assert-MockCalled Show-Browser -Exactly -Times 1 -Scope It -ParameterFilter {
+         Should -Invoke Show-Browser -Exactly -Times 1 -Scope It -ParameterFilter {
             $Url -eq "https://dev.azure.com/test/ShowProject"
          }
       }
