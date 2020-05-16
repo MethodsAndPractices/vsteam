23aa0a65b3ce1fdb36d795f633136c5b6bc59312
Donovan Brown <dbrown@microsoft.com>
2020-05-15 22:15:10 -0500

    Show-VSTeam

--- unit/test/Show-VSTeam.Tests.ps1
+++ unit/test/Show-VSTeam.Tests.ps1
@@ -1,23 +1,27 @@
 Set-StrictMode -Version Latest
 
-$here = Split-Path -Parent $MyInvocation.MyCommand.Path
-$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".")
-
-. "$here/../../Source/Classes/VSTeamVersions.ps1"
-. "$here/../../Source/Classes/VSTeamProjectCache.ps1"
-. "$here/../../Source/Private/common.ps1"
-. "$here/../../Source/Public/$sut"
-
 Describe 'Show-VSTeam' {
-   Context 'Show-VSTeam' {
-      Mock _getInstance { return 'https://dev.azure.com/test' }
+   BeforeAll {
+      $sut = (Split-Path -Leaf $PSCommandPath).Replace(".Tests.", ".")
       
-      Mock Show-Browser -Verifiable
+      . "$PSScriptRoot/../../Source/Classes/VSTeamVersions.ps1"
+      . "$PSScriptRoot/../../Source/Classes/VSTeamProjectCache.ps1"
+      . "$PSScriptRoot/../../Source/Private/common.ps1"
+      . "$PSScriptRoot/../../Source/Public/$sut"
+   }
+   
+   Context 'Show-VSTeam' {
+      BeforeAll {
+         Mock _getInstance { return 'https://dev.azure.com/test' }
 
-      Show-VSTeam
+         Mock Show-Browser -Verifiable
+
+         Show-VSTeam
+      }
 
       It 'Should open browser' {
-         Assert-VerifiableMock
+         Should -InvokeVerifiable
       }
    }
 }
+
