cba8c3c5d675c12993b821fc688508a6958a3d2b
Donovan Brown <dbrown@microsoft.com>
2020-05-16 09:02:46 -0500

    Show-VSTeam

--- unit/test/Show-VSTeam.Tests.ps1
+++ unit/test/Show-VSTeam.Tests.ps1
@@ -1,27 +1,24 @@
 Set-StrictMode -Version Latest
 
-Describe 'Show-VSTeam' {
+Describe 'VSTeam' {
    BeforeAll {
       $sut = (Split-Path -Leaf $PSCommandPath).Replace(".Tests.", ".")
-      
+
       . "$PSScriptRoot/../../Source/Classes/VSTeamVersions.ps1"
       . "$PSScriptRoot/../../Source/Classes/VSTeamProjectCache.ps1"
       . "$PSScriptRoot/../../Source/Private/common.ps1"
       . "$PSScriptRoot/../../Source/Public/$sut"
+
+      Mock _getInstance { return 'https://dev.azure.com/test' }
+
+      Mock Show-Browser -Verifiable
    }
-   
+
    Context 'Show-VSTeam' {
-      BeforeAll {
-         Mock _getInstance { return 'https://dev.azure.com/test' }
-
-         Mock Show-Browser -Verifiable
-
-         Show-VSTeam
-      }
-
       It 'Should open browser' {
+         Show-VSTeam
+         
          Should -InvokeVerifiable
       }
    }
 }
-
