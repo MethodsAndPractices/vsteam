b000fb35009645633ab23ca27d58f89cb64938c7
James O'Neill <P10506111@capita.co.uk>
2020-05-19 15:04:24 +0100

    Test did not like process Vaildator making multiple Cache  requests

--- Source/Classes/ProcessValidateAttribute.ps1
+++ Source/Classes/ProcessValidateAttribute.ps1
@@ -7,11 +7,12 @@ class ProcessValidateAttribute : ValidateArgumentsAttribute {
 
       #Do not fail on null or empty, leave that to other validation conditions
       if ([string]::IsNullOrEmpty($arguments)) {return}
-
-      if (($null -ne [VSTeamProcessCache]::GetCurrent()) -and (-not ($arguments -in [VSTeamProcessCache]::GetCurrent() )) ) {
+      #tests count HTTP calls and expect 1 from reading the cache, but for a call on each read, so only read once! # 
+      $CachedProcesses = [VSTeamProcessCache]::GetCurrent()
+      if (($null -ne $CachedProcesses) -and ($arguments -notin $CachedProcesses) ) {
          throw [ValidationMetadataException]::new(
             "'$arguments' is not a valid process. Valid processes are: '" +
-            ([VSTeamProcessCache]::GetCurrent() -join "', '") + "'")
+            ($CachedProcesses -join "', '") + "'")
       }
    }
 }
