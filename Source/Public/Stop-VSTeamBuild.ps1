c0b6d058c825f5227c211fc9ff1968597b209c09
Sebastian Schütze <sebastian.schuetze@razorspoint.com>
2020-05-21 10:50:00 +0200

    Added unit test for testing pipeline of Ids

--- Source/Public/Stop-VSTeamBuild.ps1
+++ Source/Public/Stop-VSTeamBuild.ps1
@@ -1,42 +1,35 @@
 function Stop-VSTeamBuild {
-  [CmdletBinding(SupportsShouldProcess = $true, ConfirmImpact = "Medium")]
-  param(
-     [parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
-     [Alias('BuildID')]
-     [Int] $Id,
-
-     [Parameter(Mandatory = $true, Position = 0, ValueFromPipelineByPropertyName = $true)]
-     [ProjectValidateAttribute()]
-     [ArgumentCompleter([ProjectCompleter])]
-     [string] $ProjectName
-  )
-
-  process {
-     if ($pscmdlet.ShouldProcess($Id, "Stop-VSTeamBuild")) {
-      
-      try {
-        $body = '{'
-
-        $items = New-Object System.Collections.ArrayList
-
-        if ($null -ne "`"status`": `"Cancelling`"") {
-          $items.Add("`"status`": `"Cancelling`"") > $null
-        }
-
-        if ($null -ne $items -and $items.count -gt 0) {
-          $body += ($items -join ", ")
-        }
-
-        $body += '}'
-
-        # Call the REST API
-        _callAPI -ProjectName $ProjectName -Area 'build' -Resource 'builds' -Id $Id `
-           -Method Patch -ContentType 'application/json' -body $body -Version $(_getApiVersion Build) | Out-Null
-      }
-
-      catch {
-        _handleException $_
-      }
-    }
-  }
-}
+   [CmdletBinding(SupportsShouldProcess = $true, ConfirmImpact = "Medium")]
+   param(
+      [parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+      [Alias('BuildID')]
+      [Int] $Id,
+ 
+      [Parameter(Mandatory = $true, Position = 0, ValueFromPipelineByPropertyName = $true)]
+      [ProjectValidateAttribute()]
+      [ArgumentCompleter([ProjectCompleter])]
+      [string] $ProjectName
+   )
+ 
+   process {
+      if ($pscmdlet.ShouldProcess($Id, "Stop-VSTeamBuild")) {
+       
+       try {
+ 
+          $body = @{
+             "status" = "Cancelling"
+          }
+ 
+         $bodyAsJson = $body | ConvertTo-Json -Compress -Depth 50
+ 
+         # Call the REST API
+         _callAPI -ProjectName $ProjectName -Area 'build' -Resource 'builds' -Id $Id `
+            -Method Patch -ContentType 'application/json' -body $bodyAsJson -Version $(_getApiVersion Build) | Out-Null
+       }
+ 
+       catch {
+         _handleException $_
+       }
+     }
+   }
+ }
