13e35a3775757e5a0bfd08547bbaf750d6133936
Donovan Brown <dbrown@microsoft.com>
2020-05-26 15:39:22 -0500

    Polish

--- Source/Classes/VSTeamProcessCache.ps1
+++ Source/Classes/VSTeamProcessCache.ps1
@@ -1,13 +1,14 @@
 # cache process names and URLs to reduce the number of
 # rest APIs calls needed for parameter completion / validation 
 
-#Unit tests should populate cache with expected processes ideally with 
+# Unit tests should populate cache with expected processes ideally with 
 # a mock for  Get-VSTeamProcess which returns objects with name and (optionally) URL properties 
 # we let them mock the testing freshness but really they should call invalidate 
 class VSTeamProcessCache {
    static [int] $timestamp = -1
    static [object[]] $processes = @()
    static [hashtable] $urls = @{}
+
    static [Void] Update () {     
       $list = Get-VSTeamProcess
       if ($list) {
@@ -16,25 +17,37 @@ class VSTeamProcessCache {
                [VSTeamProcessCache]::urls[$process.name] = $process.url
             }
          }
-         [VSTeamProcessCache]::processes = @() + ( $List | Select-Object -ExpandProperty Name | Sort-Object)
+         [VSTeamProcessCache]::processes = @() + ($List | Select-Object -ExpandProperty Name | Sort-Object)
       }
       else {
          [VSTeamProcessCache]::processes = @()
       }
+
       [VSTeamProcessCache]::timestamp = (Get-Date).Minute
    }
-   #"save current minute" refreshes on average after 30secs  but not after exact hours timeOfDayTotalMinutes might be a better base
+   
+   # "save current minute" refreshes on average after 30secs  but not after 
+   # exact hours timeOfDayTotalMinutes might be a better base
    static [bool] HasExpired () {
       return $([VSTeamProcessCache]::timestamp) -ne (Get-Date).Minute
    }
+   
    static [object] GetCurrent () {
-      if ([VSTeamProcessCache]::HasExpired()) { [VSTeamProcessCache]::Update() }
+      if ([VSTeamProcessCache]::HasExpired()) { 
+         [VSTeamProcessCache]::Update() 
+      }
+
       return ([VSTeamProcessCache]::processes)
    }
-   static [object] GetURl ([string]$ProcessName) {
-      if ([VSTeamProcessCache]::HasExpired()) { [VSTeamProcessCache]::Update() }
+   
+   static [object] GetURl ([string] $ProcessName) {
+      if ([VSTeamProcessCache]::HasExpired()) {
+         [VSTeamProcessCache]::Update()
+      }
+          
       return ([VSTeamProcessCache]::urls[$ProcessName])
    }
+   
    static [void] Invalidate () {
       [VSTeamProcessCache]::timestamp = -1 
    }
--- Source/Public/Get-VSTeamProcess.ps1
+++ Source/Public/Get-VSTeamProcess.ps1
@@ -1,16 +1,16 @@
 function Get-VSTeamProcess {
    [CmdletBinding(DefaultParameterSetName = 'List')]
-  # [OutputType([vsteamprocess])]
+   # [OutputType([vsteamprocess])]
    param(
-      [Parameter(ParameterSetName = 'ByName', Position=0)]
+      [Parameter(ParameterSetName = 'ByName', Position = 0)]
       [ArgumentCompleter([ProcessTemplateCompleter])]
-      [Alias('ProcessName','ProcessTemplate')]
+      [Alias('ProcessName', 'ProcessTemplate')]
       $Name = '*',
 
-      [Parameter(DontShow=$true, ParameterSetName = 'List')]
+      [Parameter(DontShow = $true, ParameterSetName = 'List')]
       [int] $Top = 100,
 
-      [Parameter(DontShow=$true, ParameterSetName = 'List')]
+      [Parameter(DontShow = $true, ParameterSetName = 'List')]
       [int] $Skip = 0,
 
       [Parameter(ParameterSetName = 'ByID')]
@@ -19,13 +19,14 @@ function Get-VSTeamProcess {
    )
    process {
       # The REST API ignores Top and Skip but allows them to be specified & the function does the same. 
-      if ($PSBoundParameters['Top','Skip'] -gt 0) {
+      if ($PSBoundParameters['Top', 'Skip'] -gt 0) {
          Write-Warning "You specified -Top $Top , -Skip $Skip These parameters are ignored and will be removed in future"
       }
+
       # Return either a single proces by ID or a list of processes
       if ($id) {
          # Call the REST API with an ID
-         $resp = _callAPI -NoProject -area 'work' -resource 'processes' -id $id  -Version $(_getApiVersion Core) 
+         $resp = _callAPI -NoProject -Area 'work' -resource 'processes' -id $id  -Version $(_getApiVersion Core) 
 
          if ($resp) {
             return [VSTeamProcess]::new($resp)
@@ -42,13 +43,14 @@ function Get-VSTeamProcess {
             # This casuses the first error to terminate this execution.
             _handleException $_
          }
-         #we just fetched all the processes so let's update the cache. Also, cache the URLS for processes
+         
+         # We just fetched all the processes so let's update the cache. Also, cache the URLS for processes
          [VSTeamProcessCache]::processes = $resp.value.name | Sort-Object
          [VSTeamProcessCache]::timestamp = (Get-Date).Minute
          $resp.value | ForEach-Object {
-            [VSTeamProcessCache]::urls[$_.name] =  (_getInstance) + "/_apis/work/processes/" + $_.TypeId
+            [VSTeamProcessCache]::urls[$_.name] = (_getInstance) + "/_apis/work/processes/" + $_.TypeId
             [VSTeamProcess]::new($_)
-         } | Where-Object {$_.name -like $Name} | Sort-Object -Property Name
+         } | Where-Object { $_.name -like $Name } | Sort-Object -Property Name
       }
    }
 }
