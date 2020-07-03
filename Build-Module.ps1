d095ce53402b9c6c80c7aec916bc7da3a540278a
Donovan Brown <dbrown@microsoft.com>
2020-07-02 19:20:09 -0500

    Getting build to work again.

--- Build-Module.ps1
+++ Build-Module.ps1
@@ -45,11 +45,11 @@ param(
 
 if ($installDep.IsPresent -or $analyzeScript.IsPresent) {
    # Load the psd1 file so you can read the required modules and install them
-   $manifest = Import-PowerShellDataFile .\Source\VSTeam.psd1
+   $manifest = Import-PowerShellDataFile .\Source\VSTeam.psd1 
 
    # Install each module
    if ($manifest.RequiredModules) {
-      $manifest.RequiredModules | ForEach-Object { if (-not (get-module $_ -ListAvailable)) { Write-Host "Installing $_"; Install-Module -Name $_ -Repository PSGallery -F -Scope CurrentUser } }
+      $manifest.RequiredModules | ForEach-Object { if (-not (get-module $_ -ListAvailable)) { Write-Host "Installing $_"; Install-Module -SkipPublisherCheck -Name $_ -Repository PSGallery -F -Scope CurrentUser } }
    }
 }
 
@@ -92,9 +92,8 @@ $newValue = ((Get-ChildItem -Path "./Source/Public" -Filter '*.ps1').BaseName |
 
 Write-Output "Publish complete to $output"
 
-
 # reload the just built module
-if ($ipmo.IsPresent -or $runTests.IsPresent) {
+if ($ipmo.IsPresent) {
 
    # module needs to be unloaded if present
    if ((Get-Module VSTeam)) {
@@ -107,51 +106,12 @@ if ($ipmo.IsPresent -or $runTests.IsPresent) {
 
 # run the unit tests with Pester
 if ($runTests.IsPresent) {
-   if ($null -eq $(Get-Module -Name Pester)) {
-      Install-Module -Name Pester -Repository PSGallery -Force -Scope CurrentUser -AllowClobber -SkipPublisherCheck
+   if ($null -eq $(Get-Module -ListAvailable Pester | Where-Object Version -like '5.*')) {
+      Write-Output "Installing Pester 5"
+      Install-Module -Name Pester -Repository PSGallery -Force -AllowPrerelease -MinimumVersion '5.0.2' -Scope CurrentUser -AllowClobber -SkipPublisherCheck
    }
 
-   $pesterArgs = @{
-      Script       = '.\unit'  
-      OutputFile   = 'test-results.xml'
-      OutputFormat = 'NUnitXml'
-      Show         = 'Fails'
-      Strict       = $true
-   }
-
-   if ($codeCoverage.IsPresent) {
-      $pesterArgs.CodeCoverage = "./Source/**/*.ps1"
-      $pesterArgs.CodeCoverageOutputFile = "coverage.xml"
-      $pesterArgs.CodeCoverageOutputFileFormat = 'JaCoCo'
-   }
-   else {
-      $pesterArgs.PassThru = $true
-   }
-
-   if ($testName) {
-
-      $pesterArgs.TestName = $testName
-
-      #passthru must be activated according to Pester docs
-      $pesterArgs.PassThru = $true
-   }
-
-   Invoke-Pester @pesterArgs 
-}
-
-# Run this last so the results can be seen even if tests were also run
-# if not the results scroll off and my not be in the buffer.
-# run PSScriptAnalyzer
-if ($analyzeScript.IsPresent) {
-   Write-Output "Starting static code analysis..."
-   if ($null -eq $(Get-Module -Name PSScriptAnalyzer)) {
-      Install-Module -Name PSScriptAnalyzer -Repository PSGallery -Force -Scope CurrentUser
-   }
-
-   $r = Invoke-ScriptAnalyzer -Path $output -Recurse
-   code analysis complete."
-$  Write-Output "Static code analysis complete."
-}  # This loads [PesterConfiguration] into scope
+   # This loads [PesterConfiguration] into scope
    Import-Module Pester -MinimumVersion 5.0.0
 
    $pesterArgs = [PesterConfiguration]::Default
@@ -174,7 +134,7 @@ $  Write-Output "Static code analysis complete."
       $pesterArgs.Filter.FullName = $testName
    }
 
-   Invoke-Pester -Configuration $pesterArgs
+   Invoke-Pester -Configuration $pesterArgs 
 }
 
 # Run this last so the results can be seen even if tests were also run
@@ -189,9 +149,4 @@ if ($analyzeScript.IsPresent) {
    $r = Invoke-ScriptAnalyzer -Path $output -Recurse
    $r | ForEach-Object { Write-Host "##vso[task.logissue type=$($_.Severity);sourcepath=$($_.ScriptPath);linenumber=$($_.Line);columnnumber=$($_.Column);]$($_.Message)" }
    Write-Output "Static code analysis complete."
-}
-
-# run integration tests with Pester
-if ($runIntegrationTests.IsPresent) {
-   Start-IntegrationTests
 }
