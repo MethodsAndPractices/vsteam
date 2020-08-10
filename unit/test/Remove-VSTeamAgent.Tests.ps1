cf628b97cdd9504d62595c715e97ea733e9c7f03
Donovan Brown <dbrown@microsoft.com>
2020-08-10 11:38:18 -0500

    Clean up a tests

--- unit/test/Remove-VSTeamAgent.Tests.ps1
+++ unit/test/Remove-VSTeamAgent.Tests.ps1
@@ -2,45 +2,42 @@ Set-StrictMode -Version Latest
 
 Describe 'VSTeamAgent' {
    BeforeAll {
-      Add-Type -Path "$PSScriptRoot/../../dist/bin/vsteam-lib.dll"
-      
       $sut = (Split-Path -Leaf $PSCommandPath).Replace(".Tests.", ".")
    
-      . "$PSScriptRoot/../../Source/Classes/VSTeamVersions.ps1"
       . "$PSScriptRoot/../../Source/Private/common.ps1"
       . "$PSScriptRoot/../../Source/Public/$sut"
-
-      # Prime the project cache with an empty list. This will make sure
-      # any project name used will pass validation and Get-VSTeamProject 
-      # will not need to be called.
-      [vsteam_lib.ProjectCache]::Update([string[]]@())
    
-      Mock _getInstance { return 'https://dev.azure.com/test' } -Verifiable
-      [VSTeamVersions]::DistributedTaskReleased = '1.0-unitTest'
+      Mock _handleException
+      Mock _getApiVersion { return '1.0-unitTests' }
    }
 
    Context 'Remove-VSTeamAgent by ID' {
       BeforeAll {
-         Mock Invoke-RestMethod
+         Mock _callAPI
       }
 
       It 'should remove the agent with passed in Id' {
          Remove-VSTeamAgent -Pool 36 -Id 950 -Force
 
-         Should -Invoke Invoke-RestMethod -Exactly -Scope It -Times 1 -ParameterFilter {
-            $Method -eq 'Delete' -and
-            $Uri -eq "https://dev.azure.com/test/_apis/distributedtask/pools/36/agents/950?api-version=$(_getApiVersion DistributedTaskReleased)"
+         Should -Invoke _callAPI -Exactly -Scope It -Times 1 -ParameterFilter {
+            $Method -eq 'DELETE' -and
+            $Area -eq 'distributedtask/pools/36' -and
+            $Resource -eq 'agents' -and
+            $ID -eq 950 -and
+            $Version -eq $(_getApiVersion DistributedTaskReleased)
          }
       }
    }
 
    Context 'Remove-VSTeamAgent throws' {
       BeforeAll {
-         Mock Invoke-RestMethod { throw 'boom' }
+         Mock _callAPI { throw 'boom' }
       }
 
       It 'should remove the agent with passed in Id' {
-         { Remove-VSTeamAgent -Pool 36 -Id 950 -Force } | Should -Throw
+         Remove-VSTeamAgent -Pool 36 -Id 950 -Force
+         
+         Should -Invoke _handleException -Exactly -Scope It -Times 1
       }
    }
 }
