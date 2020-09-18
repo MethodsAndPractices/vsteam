1a35d15f90ea65a92b032b1cf954635ff4ebcd14
Donovan Brown <dbrown@microsoft.com>
2020-09-18 15:38:31 -0500

    Fixed newline to make cross platform.

--- Tests/library/Provider/DescriptorTests.cs
+++ Tests/library/Provider/DescriptorTests.cs
@@ -12,7 +12,8 @@ namespace vsteam_lib.Test.Provider
       {
          // Arrange
          var obj = BaseTests.LoadJson("descriptor.scope.TestProject.json", false);
-         var expectedLinks = "Self: https://vssps.dev.azure.com/test/_apis/Graph/Descriptors/010d06f0-00d5-472a-bb47-58947c230876\r\nStorageKey: https://vssps.dev.azure.com/test/_apis/Graph/StorageKeys/scp.ZGU5ODYwOWEtZjRiMC00YWEzLTgzOTEtODI4ZDU2MDI0MjU2\r\nSubject: https://vssps.dev.azure.com/test/_apis/Graph/Scopes/scp.ZGU5ODYwOWEtZjRiMC00YWEzLTgzOTEtODI4ZDU2MDI0MjU2\r\n";
+         var n = System.Environment.NewLine;
+         var expectedLinks = $"Self: https://vssps.dev.azure.com/test/_apis/Graph/Descriptors/010d06f0-00d5-472a-bb47-58947c230876{n}StorageKey: https://vssps.dev.azure.com/test/_apis/Graph/StorageKeys/scp.ZGU5ODYwOWEtZjRiMC00YWEzLTgzOTEtODI4ZDU2MDI0MjU2{n}Subject: https://vssps.dev.azure.com/test/_apis/Graph/Scopes/scp.ZGU5ODYwOWEtZjRiMC00YWEzLTgzOTEtODI4ZDU2MDI0MjU2{n}";
 
          // Act
          var target = new Descriptor(obj[0]);
