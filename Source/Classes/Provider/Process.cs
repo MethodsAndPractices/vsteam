96988e460e8bcc3f5f4dd06e501ea89a83e2407f
Donovan Brown <dbrown@microsoft.com>
2020-10-15 18:38:06 -0500

    Fixing the Process.cs class. Added tests to verify the changes to the Process class.

--- Source/Classes/Provider/Process.cs
+++ Source/Classes/Provider/Process.cs
@@ -1,4 +1,6 @@
-﻿using System.Management.Automation;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
 using System.Xml.Serialization;
 using vsteam_lib.Provider;
 
@@ -15,11 +17,13 @@ namespace vsteam_lib
       public string Type { get; set; }
       public string ProcessTemplate => this.Name;
       public string ParentProcessTypeId { get; set; }
-      public string[] Projects { get; set; }
+      public IEnumerable<string> Projects { get; }
 
       public Process(PSObject obj) :
          base(obj, obj.GetValue("name"), obj.GetValue("id"), null)
       {
+         this.Projects = obj.GetValue<object[]>("Projects")?.Select(o => o.ToString()).ToArray();
+
          // Depending on what API was used you might get and ID or a TypeId back.
          // If you get a TypeId you need to set the ID now
          if (string.IsNullOrEmpty(this.Id))
