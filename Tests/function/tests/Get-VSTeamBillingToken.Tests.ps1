20092407019f6b4a9d6d964ba40a8c6744fa08e8
Sebastian Schütze <sebastian.schuetze@razorspoint.com>
2020-10-18 14:10:37 +0200

    fixed url in test

--- Tests/function/tests/Get-VSTeamBillingToken.Tests.ps1
+++ Tests/function/tests/Get-VSTeamBillingToken.Tests.ps1
@@ -6,7 +6,7 @@ Describe 'VSTeamBilling' -Tag 'unit', 'billing' {
 
       # Set the account to use for testing. A normal user would do this
       # using the Set-VSTeamAccount function.
-      Mock _getInstance { return 'https://dev.azure.com/test' }
+      Mock _getInstance { return 'https://dev.azure.com' }
 
       Mock Invoke-RestMethod { Write-Host $args; Open-SampleFile 'Get-VSTeamBillingToken.json' }
    }
@@ -24,7 +24,7 @@ Describe 'VSTeamBilling' -Tag 'unit', 'billing' {
             $Method -eq 'Post' -and
             $Body -like '*"namedTokenId":"CommerceDeploymentProfile"*' -and
             $Body -like '*"tokenType":0*' -and
-            $Uri -eq "https://dev.azure.com/test/_apis/WebPlatformAuth/SessionToken?api-version=3.2-preview.1"
+            $Uri -eq "https://dev.azure.com/_apis/WebPlatformAuth/SessionToken?api-version=3.2-preview.1"
          }
       }
    }
