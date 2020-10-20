c9f0ac986ef076593f74f1d9327a4b727e336696
James O'Neill <P10506111@capita.co.uk>
2020-10-20 22:22:10 +0100

    More flexible field commands

--- Source/Public/Add-VSTeamField.ps1
+++ Source/Public/Add-VSTeamField.ps1
@@ -1,17 +1,17 @@
-function Add-VSTeamField { 
+function Add-VSTeamField {
    [CmdletBinding(DefaultParameterSetName='NoPicklist',SupportsShouldProcess=$true,ConfirmImpact='High')]
    Param(
       [Parameter(Mandatory=$true,Position=0)]
       [string]$Name,
-      
+
       [string]$ReferenceName,
-      
+
       [ValidateSet('boolean','dateTime','double', 'html','identity','integer','string')]
       [string]$Type = 'string',
-      
+
       [ValidateSet('workItem','workItemLink','workItemTypeExtension','tree','none')]
       $Usage='workItem',
-      
+
       [string]$Description,
 
       [ArgumentCompleter([vsteam_lib.PicklistCompleter])]
@@ -31,55 +31,47 @@ function Add-VSTeamField {
       [switch]$Force
    )
    process {
-      $body =  @{'name'     = $Name 
+      $body =  @{'name'     = $Name
                'type'     = $Type.ToLower()
                'usage'    = $usage.ToLower()
-               'readOnly' = $ReadOnly -as [bool] } 
-
+               'readOnly' = $ReadOnly -as [bool] }
       if ($PSBoundParameters.ContainsKey('PicklistID') ) {
          if ($PicklistID.psobject.members.name -contains "id") {$PicklistID=$PicklistID.id}
          if ($PicklistID  -notmatch "[0-9A-F]{8}-([0-9A-F]{4}-){3}[0-9A-F]{12}"  ) {
-            Write-Warning "$PicklistID does not appear to be a valid picklist" ; return 
+            Write-Warning "$PicklistID does not appear to be a valid picklist" ; return
          }
          if ($PicklistID -is [array]) {
-               $body['picklistId']=$PicklistID[0]
+               $body['picklistId']    = $PicklistID[0]
          }
-         else {$body['picklistId']          = $PicklistID}
+         else {$body['picklistId']    = $PicklistID}
          $body['isPicklist']          = $true
          $body['isPicklistSuggested'] = $IsPickListSuggested -as [bool]
       }
-
       if ($PSBoundParameters.ContainsKey('Description')) {
             $body['description'] = $Description
-      }
 
+      }
       if ($PSBoundParameters.ContainsKey('ReferenceName')) {
             $body['referenceName'] = $ReferenceName
       }
-
       if ($PSBoundParameters.ContainsKey('NotQueryable')) {
             $body['isQueryable'] = -not $NotQueryable
       }
-
       if ($PSBoundParameters.ContainsKey('NoSort') ) {
-            $body['canSortBy'] = -not  $NoSort 
+            $body['canSortBy'] = -not  $NoSort
       }
-
       $apiParams = @{
-            method      = 'Post' 
+            method      = 'Post'
             area        = 'wit'
-            resource    = 'fields' 
+            resource    = 'fields'
             version     = _getApiVersion Processes
             body        = ConvertTo-Json $body
             ContentType = 'application/json'
       }
-
       if ($Force -or $PSCmdlet.ShouldProcess($Name,'Add field') ) {
          #call the REST API
          $resp = _callAPI @apiParams
-
          $resp.psobject.TypeNames.Insert(0,'vsteam_lib.Field')
-
          return $resp
       }
    }
--- Source/Public/Get-VSTeamField.ps1
+++ Source/Public/Get-VSTeamField.ps1
@@ -9,10 +9,13 @@ function Get-VSTeamField {
    )
    process {
       if ($ReferenceName ) {
-            if ($ReferenceName.psobject.Members.Name -contains "ReferenceName") {
-                  $ReferenceName = $ReferenceName.ReferenceName
+         $resp = @()
+         foreach ($r in $ReferenceName){
+            if ($r.psobject.Members.Name -contains "ReferenceName") {
+                  $r = $r.ReferenceName
             }
-            $resp =  _callAPI -area wit -resource fields -id $ReferenceName
+            $resp +=  _callAPI -area wit -resource fields -id $r
+         }
       }
       else {$resp = (_callAPI -area wit -resource fields).value}
 
