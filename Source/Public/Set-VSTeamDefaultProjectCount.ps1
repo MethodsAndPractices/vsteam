5ae5a6567a6511e19353c5625fb73dd85baf3746
Donovan Brown <dbrown@microsoft.com>
2020-12-05 18:08:57 -0600

    Reduced redundant code.

--- Source/Public/Set-VSTeamDefaultProjectCount.ps1
+++ Source/Public/Set-VSTeamDefaultProjectCount.ps1
@@ -8,35 +8,7 @@ function Set-VSTeamDefaultProjectCount {
       [int] $Count
    )
    DynamicParam {
-      $dp = New-Object System.Management.Automation.RuntimeDefinedParameterDictionary
-
-      # Only add these options on Windows Machines
-      if (_isOnWindows) {
-         $ParameterName = 'Level'
-         # Create the collection of attributes
-         $AttributeCollection = New-Object System.Collections.ObjectModel.Collection[System.Attribute]
-         # Create and set the parameters' attributes
-         $ParameterAttribute = New-Object System.Management.Automation.ParameterAttribute
-         $ParameterAttribute.Mandatory = $false
-         $ParameterAttribute.HelpMessage = "On Windows machines allows you to store the default project at the process, user or machine level. Not available on other platforms."
-         # Add the attributes to the attributes collection
-         $AttributeCollection.Add($ParameterAttribute)
-         # Generate and set the ValidateSet
-         if (_testAdministrator) {
-            $arrSet = "Process", "User", "Machine"
-         }
-         else {
-            $arrSet = "Process", "User"
-         }
-         $ValidateSetAttribute = New-Object System.Management.Automation.ValidateSetAttribute($arrSet)
-         # Add the ValidateSet to the attributes collection
-         $AttributeCollection.Add($ValidateSetAttribute)
-         # Create and return the dynamic parameter
-         $RuntimeParameter = New-Object System.Management.Automation.RuntimeDefinedParameter($ParameterName, [string], $AttributeCollection)
-         $dp.Add($ParameterName, $RuntimeParameter)
-      }
-
-      return $dp
+      _buildLevelDynamicParam
    }
 
    begin {
