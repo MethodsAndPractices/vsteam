de25323404d0de19706bebdb0aa0afa571056c9a
Donovan Brown <dbrown@microsoft.com>
2020-12-18 15:22:55 -0600

    Formatting code to match rest of project.

--- Source/Classes/Attribute/ColorTransformToHexAttribute.cs
+++ Source/Classes/Attribute/ColorTransformToHexAttribute.cs
@@ -1,30 +1,39 @@
 ﻿using System.Diagnostics.CodeAnalysis;
-using System.Management.Automation;
 using System.Drawing;
+using System.Management.Automation;
 using System.Text.RegularExpressions;
+
 namespace vsteam_lib
 {
-   public class ColorTransformToHexAttribute : ArgumentTransformationAttribute {
+   public sealed class ColorTransformToHexAttribute : ArgumentTransformationAttribute
+   {
       [ExcludeFromCodeCoverage]
-      public override object Transform(EngineIntrinsics engineIntrinsics, object InputData) {
-         try {
-            if (InputData is string) {
-               string s = (string)InputData;
-               Regex sixHexDigits = new Regex(@"^#?([\da-f]{6})$",RegexOptions.IgnoreCase);
-               if (sixHexDigits.Match(s).Success ) {
-                  InputData = sixHexDigits.Replace(s,"$1");
+      public override object Transform(EngineIntrinsics engineIntrinsics, object InputData)
+      {
+         try
+         {
+            if (InputData is string s)
+            {
+               var sixHexDigits = new Regex(@"^#?([\da-f]{6})$", RegexOptions.IgnoreCase);
+
+               if (sixHexDigits.Match(s).Success)
+               {
+                  InputData = sixHexDigits.Replace(s, "$1");
                }
-               else {
-                  InputData =  Color.FromName(s);
-               }                
+               else
+               {
+                  InputData = Color.FromName(s);
+               }
             }
-            if (InputData is Color) {
-               Color c = (Color)InputData;
-               InputData = (string.Format("{0:x2}{1:x2}{2:x2}",c.R, c.G, c.B) );
+
+            if (InputData is Color c)
+            {
+               InputData = (string.Format("{0:x2}{1:x2}{2:x2}", c.R, c.G, c.B));
             }
          }
-         catch {
-            throw (new ParameterBindingException((InputData as string) + " could not be transformed into a color.") ); 
+         catch
+         {
+            throw new ParameterBindingException((InputData as string) + " could not be transformed into a color.");
          }
 
          return (InputData);
