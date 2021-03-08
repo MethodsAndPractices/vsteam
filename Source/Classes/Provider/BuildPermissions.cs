676143f9fc2277bd02db5aaf75f31f3040661749
Sebastian Schütze <sebastian.schuetze@razorspoint.com>
2021-03-08 16:00:57 +0100

    Added cmdlet Add-VSTeamBuildPermission

---
+++ Source/Classes/Provider/BuildPermissions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace vsteam_lib
+{
+   [Flags]
+   public enum BuildPermissions
+   {
+      ViewBuilds = 1,
+      EditBuildQuality = 2,
+      RetainIndefinitely = 4,
+      DeleteBuilds = 8,
+      ManageBuildQualities = 16,
+      DestroyBuilds = 32,
+      UpdateBuildInformation = 64,
+      QueueBuilds = 128,
+      ManageBuildQueue = 256,
+      StopBuilds = 512,
+      ViewBuildDefinition = 1024,
+      EditBuildDefinition = 2048,
+      DeleteBuildDefinition = 4096,
+      OverrideBuildCheckInValidation = 8192,
+      AdministerBuildPermissions = 16384
+   }
+}
