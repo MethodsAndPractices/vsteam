478181a0387c6c0e9a057084ba9374656fecac9f
Sebastian Schütze <sebastian.schuetze@razorspoint.com>
2021-03-14 18:11:42 +0100

    optimized base completer

--- Source/Classes/Completer/BaseCompleter.cs
+++ Source/Classes/Completer/BaseCompleter.cs
@@ -11,63 +11,71 @@ using System.Runtime.CompilerServices;
 
 namespace vsteam_lib
 {
-   public abstract class BaseCompleter : IArgumentCompleter
-   {
-      protected readonly IPowerShell _powerShell;
+    public abstract class BaseCompleter : IArgumentCompleter
+    {
+        protected readonly IPowerShell _powerShell;
 
-      /// <summary>
-      /// This constructor is used during unit testings
-      /// </summary>
-      /// <param name="powerShell">fake instance of IPowerShell used for testing</param>
-      protected BaseCompleter(IPowerShell powerShell) { this._powerShell = powerShell; }
+        /// <summary>
+        /// This constructor is used during unit testings
+        /// </summary>
+        /// <param name="powerShell">fake instance of IPowerShell used for testing</param>
+        protected BaseCompleter(IPowerShell powerShell) { this._powerShell = powerShell; }
 
-      /// <summary>
-      /// This constructor is used when running in a PowerShell session. It cannot be
-      /// loaded in a unit test.
-      /// </summary>
-      [ExcludeFromCodeCoverage]
-      protected BaseCompleter() : this(new PowerShellWrapper(RunspaceMode.CurrentRunspace)) { }
+        /// <summary>
+        /// This constructor is used when running in a PowerShell session. It cannot be
+        /// loaded in a unit test.
+        /// </summary>
+        [ExcludeFromCodeCoverage]
+        protected BaseCompleter() : this(new PowerShellWrapper(RunspaceMode.CurrentRunspace)) { }
 
-      public abstract IEnumerable<CompletionResult> CompleteArgument(string commandName,
-                                                                     string parameterName,
-                                                                     string wordToComplete,
-                                                                     CommandAst commandAst,
-                                                                     IDictionary fakeBoundParameters);
+        public abstract IEnumerable<CompletionResult> CompleteArgument(string commandName,
+                                                                       string parameterName,
+                                                                       string wordToComplete,
+                                                                       CommandAst commandAst,
+                                                                       IDictionary fakeBoundParameters);
 
-      protected static void SelectValues(string wordToComplete, IEnumerable<string> words, List<CompletionResult> values)
-      {
-         foreach (var word in words)
-         {
-            if (string.IsNullOrEmpty(wordToComplete) || word.StartsWith(wordToComplete, true, CultureInfo.InvariantCulture))
+        protected static void SelectValues(string wordToComplete, IEnumerable<string> words, List<CompletionResult> values)
+        {
+            foreach (var word in words)
             {
-               values.Add(GetCompletionResult(wordToComplete, word));
+                if (string.IsNullOrEmpty(wordToComplete) || word.StartsWith(wordToComplete, true, CultureInfo.InvariantCulture))
+                {
+                    values.Add(GetCompletionResult(word));
+                }
             }
-         }
-      }
+        }
 
-      private static CompletionResult GetCompletionResult(string wordToComplete, string word, string wordLabel = null, CompletionResultType resultType = CompletionResultType.Text)
-      {
-         if (wordLabel == null)
-         {
-            wordLabel = word;
-         }
-         // Only wrap in single quotes if they have a space. This makes it easier
-         // to use on macOs
-         return (new CompletionResult(word.Contains(" ") ? $"'{word}'" : word, wordLabel, resultType, wordLabel + ": " + word));
 
-      }
+        private static CompletionResult GetCompletionResult(string word)
+        {
+            return GetCompletionResult(word,word,CompletionResultType.Text);
+        }
 
-      protected static void SelectValues(string wordToComplete,
-                                          Dictionary<string, string> words,
-                                          List<CompletionResult> values)
-      {
-         foreach (var word in words)
-         {
-            if (string.IsNullOrEmpty(wordToComplete) || word.Key.StartsWith(wordToComplete, true, CultureInfo.InvariantCulture))
+        private static CompletionResult GetCompletionResult(string word, string wordLabel, CompletionResultType resultType)
+        {
+            // Only wrap in single quotes if they have a space. This makes it easier
+            // to use on macOs
+            return (new CompletionResult(word.Contains(" ") ? $"'{word}'" : word, wordLabel, resultType, wordLabel + ": " + word));
+
+        }
+
+        protected static void SelectValues(string wordToComplete,
+                                            Dictionary<string, string> words,
+                                            List<CompletionResult> values)
+        {
+            foreach (var word in words)
             {
-               values.Add(GetCompletionResult(wordToComplete, word.Key, word.Value, CompletionResultType.Text));
+                var keyToSearch = word.Key.ToLower();
+                var valueToSearch = word.Value.ToLower();
+
+                if (string.IsNullOrEmpty(wordToComplete) ||
+                    keyToSearch.Contains(wordToComplete) ||
+                    valueToSearch.Contains(wordToComplete)
+                    )
+                {
+                    values.Add(GetCompletionResult(word.Key, word.Value, CompletionResultType.Text));
+                }
             }
-         }
-      }
-   }
+        }
+    }
 }
