f51b209d063a2c9ac0b414cf5373795ecb851a13
Sebastian Schütze <sebastian.schuetze@razorspoint.com>
2022-01-12 23:47:55 +0000

    added notification messages on module load

--- Source/VSTeam.psm1
+++ Source/VSTeam.psm1
@@ -15,6 +15,17 @@
 # Load the correct version of the environment variable
 Set-VSTeamAPIVersion -Target $([vsteam_lib.Versions]::Version)
 
+#compare versions and notify user
+$ghLatestRelease = Invoke-WebRequest "https://api.github.com/repos/MethodsAndPractices/vsteam/releases/latest" | ConvertFrom-Json -Depth 20
+[version]$latestVersion = $ghLatestRelease.tag_name -replace "v", ""
+[version]$currentVersion = $([vsteam_lib.Versions]::ModuleVersion)
+if ($currentVersion -lt $latestVersion) {
+   Write-Host "New version available: $latestVersion"
+   Write-Host "Run: Update-Module -Name VSTeam -RequiredVersion $latestVersion"
+}
+
+Write-Host "Warning: Breaking changes coming with Version 8.0.0. Support for TFS 2017 and 2018 will be dropped.`nSee: https://github.com/MethodsAndPractices/vsteam/issues/438" -ForegroundColor Yellow
+
 # Check to see if the user stored the default project in an environment variable
 if ($null -ne $env:TEAM_PROJECT) {
    # Make sure the value in the environment variable still exisits.
