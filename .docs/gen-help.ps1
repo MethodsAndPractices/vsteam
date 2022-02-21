48e3bea1f42f0fc446d5e9d544cc1e0c68583db2
Sebastian S Schütze <sebastian.s.schuetze@deutschebahn.com>
2022-02-21 22:48:24 +0100

    - fixing related links for module help - optimizing hep generator script

--- .docs/gen-help.ps1
+++ .docs/gen-help.ps1
@@ -1,24 +1,39 @@
 Write-Verbose 'Clearing old files'
 
-if ((Test-Path ..\docs) -eq $false) {
-   New-Item -ItemType Directory -Name ..\docs | Out-Null
+$path = $PSScriptRoot
+
+$docsPath = "$path/../docs"
+
+if ((Test-Path $docsPath) -eq $false) {
+   New-Item -ItemType Directory -Name $docsPath | Out-Null
 }
 
-Get-ChildItem ..\docs | Remove-Item
+Get-ChildItem $docsPath | Remove-Item
 
 Write-Verbose 'Merging Markdown files'
-if(-not (Get-Module Trackyon.Markdown -ListAvailable)) {
+if (-not (Get-Module Trackyon.Markdown -ListAvailable)) {
    Install-Module Trackyon.Markdown -Scope CurrentUser -Force
 }
 
-merge-markdown $PSScriptRoot $PSScriptRoot\..\docs
+merge-markdown $path $docsPath
 
 Write-Verbose 'Creating new file'
 
-if(-not (Get-Module platyPS -ListAvailable)) {
+if (-not (Get-Module platyPS -ListAvailable)) {
    Install-Module platyPS -Scope CurrentUser -Force
 }
 
-$helpOutput = New-ExternalHelp ..\docs -OutputPath ..\Source\en-US -Force | Out-String
+$helpFilePath = "$path/../Source/en-US"
+
+[System.IO.FileInfo]$helpOutput = New-ExternalHelp $docsPath -OutputPath $helpFilePath -Force | Out-String
+
+# below fixes broken links from the module help file
+# it will link to the docs website directly
+[xml]$xmlMaml = Get-Content "$helpFilePath/VSTeam-Help.xml"
+$relatedLinks = $xmlMaml.helpItems.command.relatedLinks.navigationLink
+$relatedLinks | ForEach-Object {
+   $_.uri = "https://methodsandpractices.github.io/vsteam-docs/docs/modules/vsteam/commands/$($_.linkText)"
+}
+$xmlMaml.Save("$helpFilePath/VSTeam-Help.xml")
 
 Write-Verbose $helpOutput
