063277290b2ea2f7fa5eb1e45232e902b13e53df
Sebastian Schütze <sebastian.schuetze@razorspoint.com>
2022-02-21 22:51:49 +0100

    Fixed unit tests not running locally (#451)

--- Tests/function/tests/Add-VSTeamWiki.Tests.ps1
+++ Tests/function/tests/Add-VSTeamWiki.Tests.ps1
@@ -2,37 +2,38 @@ Set-StrictMode -Version Latest
 
 Describe 'VSTeamWiki' {
    BeforeAll {
-      $FakeOrg = 'myOrg'           
-      
+      $FakeOrg = 'myOrg'
+
       . "$PSScriptRoot\_testInitialize.ps1" $PSCommandPath
-      
-      Mock _getInstance { return "https://dev.azure.com/$($FakeOrg)" }      
+      . "$baseFolder/Source/Public/Get-VSTeamProject.ps1"
+
+      Mock _getInstance { return "https://dev.azure.com/$($FakeOrg)" }
       Mock _getApiVersion { return '1.0-unitTests' } -ParameterFilter { $Service -eq 'Wiki' }
-      Mock Get-VSTeamProject {return Open-SampleFile 'Get-VSTeamProject.json' }      
+      Mock Get-VSTeamProject { return Open-SampleFile 'Get-VSTeamProject.json' }
    }
 
    Context 'Add-VSTeamWiki' {
-      BeforeAll {         
-         Mock Invoke-RestMethod { Open-SampleFile 'Get-VSTeamWiki.json' -Index 0 } -ParameterFilter { 
-            ($body | ConvertFrom-Json -Depth 10).Type -eq 'codeWiki'  
+      BeforeAll {
+         Mock Invoke-RestMethod { Open-SampleFile 'Get-VSTeamWiki.json' -Index 0 } -ParameterFilter {
+            ($body | ConvertFrom-Json -Depth 10).Type -eq 'codeWiki'
          }
-         Mock Invoke-RestMethod { Open-SampleFile 'Get-VSTeamWiki.json' -Index 1 } -ParameterFilter { 
+         Mock Invoke-RestMethod { Open-SampleFile 'Get-VSTeamWiki.json' -Index 1 } -ParameterFilter {
             ($body | ConvertFrom-Json -Depth 10).Type -eq 'projectWiki'
          }
       }
       It 'Not Specifying wiki type should defult to projectWiki' {
          ## Act
          Add-VSTeamWiki -ProjectName 'project' -Name 'myWiki2'
-         
+
          ## Assert
          Should -Invoke Invoke-RestMethod -Exactly -Times 1 -Scope It -ParameterFilter {
-            $Uri -eq "https://dev.azure.com/$($FakeOrg)/_apis/wiki/wikis?api-version=$(_getApiVersion Wiki)"  -and
+            $Uri -eq "https://dev.azure.com/$($FakeOrg)/_apis/wiki/wikis?api-version=$(_getApiVersion Wiki)" -and
             $Method -eq 'POST' -and
             ($Body | ConvertFrom-Json -Depth 10).Type -eq 'ProjectWiki' -and
             ($Body | ConvertFrom-Json -Depth 10).Name -eq 'myWiki2'
          }
       }
-            
+
       It 'Specifying type repositoryId, branch and mapped Path should create a codeWiki type' {
          ## Act
          Add-VSTeamWiki -ProjectName 'project' `
@@ -40,16 +41,16 @@ Describe 'VSTeamWiki' {
             -repositoryId 1111 `
             -branch 'main' `
             -mappedPath '/.Docs'
-         
-            ## Assert
+
+         ## Assert
          Should -Invoke Invoke-RestMethod -Exactly -Times 1 -Scope It -ParameterFilter {
-            $Uri -eq "https://dev.azure.com/$($FakeOrg)/_apis/wiki/wikis?api-version=$(_getApiVersion Wiki)"  -and
+            $Uri -eq "https://dev.azure.com/$($FakeOrg)/_apis/wiki/wikis?api-version=$(_getApiVersion Wiki)" -and
             $Method -eq 'POST' -and
             ($Body | ConvertFrom-Json -Depth 10).Type -eq 'codeWiki' -and
             ($Body | ConvertFrom-Json -Depth 10).Name -eq 'myWiki1' -and
             ($Body | ConvertFrom-Json -Depth 10).RepositoryId -eq 1111 -and
             ($Body | ConvertFrom-Json -Depth 10).Version.Version -eq 'main' -and
-            ($Body | ConvertFrom-Json -Depth 10).MappedPath -eq '/.Docs'            
+            ($Body | ConvertFrom-Json -Depth 10).MappedPath -eq '/.Docs'
          }
       }
    }
