e27cfa2fc1dd1b6cc96c8dec65c71d90a19dab98
mnieto <m.nieto@kevull.com>
2023-07-07 21:31:28 +0200

    document and test Update-VSTeamWorkItem with relations

--- Tests/function/tests/Update-VSTeamWorkItem.Tests.ps1
+++ Tests/function/tests/Update-VSTeamWorkItem.Tests.ps1
@@ -3,6 +3,7 @@ Set-StrictMode -Version Latest
 Describe 'VSTeamWorkItem' {
    BeforeAll {
       . "$PSScriptRoot\_testInitialize.ps1" $PSCommandPath
+      . "$baseFolder/Source/Public/New-VSTeamWorkItemRelation.ps1"      
 
       Mock _getInstance { return 'https://dev.azure.com/test' }
    }
@@ -10,6 +11,8 @@ Describe 'VSTeamWorkItem' {
    Context 'Update-VSTeamWorkItem' {
       BeforeAll {
          Mock Invoke-RestMethod { Open-SampleFile 'Get-VSTeamWorkItem-Id16.json' }
+         Mock Invoke-RestMethod { Open-SampleFile 'Get-VSTeamWorkItem-Id16-After.json' } -ParameterFilter { $relations -ne $null }
+         Mock New-VSTeamWorkItemRelation { return [PSCustomObject]@{ Id = 80; RelationType = 'System.LinkTypes.Hierarchy-Reverse'; Operation = 'add'; Index = '-' }} -ParameterFilter { $id -eq 80 }
       }
 
       It 'Without Default Project should update work item' {
@@ -123,6 +126,31 @@ Describe 'VSTeamWorkItem' {
 
          ## Act / Assert
          { Update-VSTeamWorkItem 1 -Title Test1 -Description Testing -AdditionalFields $additionalFields } | Should -Throw
+      }
+
+      It 'With relations should update relations' {
+         ## Arrange
+         $Global:PSDefaultParameterValues["*-vsteam*:projectName"] = 'test'
+         $relations = New-VSTeamWorkItemRelation -Id 80 -RelationType Parent
+
+         ## Act / Assert
+         $wi = Update-VSTeamWorkItem 1 -Title Test1 -Description Testing -Relations $relations -Force
+         $wi.Fields."System.Parent" | Should -Be 80
+         Should -Invoke Invoke-RestMethod -Exactly -Scope It -Times 1 -ParameterFilter {
+            $Method -eq 'Patch' -and
+            $Body -like '`[*' -and # Make sure the body is an array
+            $Body -like '*Test1*' -and
+            $Body -like '*Testing*' -and
+            $Body -like '*/fields/System.Title*' -and
+            $Body -like '*/fields/System.Description*' -and
+            $Body -like '*/relations/-*' -and
+            $Body -like '*`]' -and # Make sure the body is an array
+            $ContentType -eq 'application/json-patch+json; charset=utf-8' -and
+            $Uri -eq "https://dev.azure.com/test/_apis/wit/workitems/1?api-version=$(_getApiVersion Core)"
+         }
+
+
+
       }
    }
 }
