5fa5349a1f403db45b4a51dfa092704a7babe825
mnieto <m.nieto@kevull.com>
2023-07-17 02:20:37 +0200

    function Add-VSTeamWorkItemRelation

---
+++ Source/Public/Add-VSTeamWorkItemRelation.ps1
@@ -0,0 +1,20 @@
+function Add-VSTeamWorkItemRelation {
+   [CmdletBinding(SupportsShouldProcess, ConfirmImpact = "Medium", HelpUri = 'https://methodsandpractices.github.io/vsteam-docs/docs/modules/vsteam/commands/Add-VSTeamWorkItemRelation')]
+   param (
+      [Parameter(Mandatory, Position=0, ValueFromPipeline, ValueFromPipelineByPropertyName)]
+      [int[]]$Id,
+      [ArgumentCompleter([vsteam_lib.WorkItemRelationTypeCompleter])]
+      [Parameter(Mandatory, Position=1)]
+      [string]$RelationType,
+      [Parameter(Mandatory, Position=2)]
+      [int]$OfRelatedId,
+      [switch]$Force
+   )
+   
+   process {
+      $relations = $Id | New-VSTeamWorkItemRelation -RelationType $RelationType
+      if ($Force -or $pscmdlet.ShouldProcess($Id, "Update-WorkItem")) {
+         Update-VSTeamWorkItem -Id $OfRelatedId -Relations $relations
+      }
+   }
+}
