1608dbae66e803d2c28e9958693e634cf008bf3d
Sebastian Schütze <sebastian.schuetze@razorspoint.com>
2023-10-13 14:19:07 +0200

    added possibility to get all banners

--- Source/Public/Get-VSTeamBanner.ps1
+++ Source/Public/Get-VSTeamBanner.ps1
@@ -1,14 +1,19 @@
 function Get-VSTeamBanner {
    [CmdletBinding()]
    param(
-      [Parameter(Mandatory = $true)]
+      [Parameter(Mandatory = $false)]
       [string]$Id
    )
    process {
 
-      $existingBanner = (Invoke-VSTeamRequest -method GET -area 'settings' -resource "entries/host/GlobalMessageBanners/$Id" -version '3.2-preview').value
-      if ($null -eq $existingBanner) { throw "No banner found with ID $Id" }
+      $existingBanners = $null
+      if($Id){
+         $existingBanners = (Invoke-VSTeamRequest -method GET -area 'settings' -resource "entries/host/GlobalMessageBanners/$Id" -version '3.2-preview').value
+         if ($null -eq $existingBanner) { throw "No banner found with ID $Id" }
+      }else{
+         $existingBanners = (Invoke-VSTeamRequest -method GET -area 'settings' -resource "entries/host/GlobalMessageBanners" -version '3.2-preview').value
+      }      
 
-      return $existingBanner
+      return $existingBanners
    }
 }
