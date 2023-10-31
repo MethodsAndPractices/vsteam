7c247c1730f4b03e55bfa9d99dd6e3f8d7664a32
Sebastian S Schütze <sebastian.s.schuetze@deutschebahn.com>
2023-10-31 16:43:52 +0100

    build: simplyfied version script to only be used during non production

--- tools/scripts/Set-VersionNumber.ps1
+++ tools/scripts/Set-VersionNumber.ps1
@@ -1,33 +1,32 @@
-[CmdletBinding(DefaultParameterSetName = "PublishPSGallery", SupportsShouldProcess)]
+[CmdletBinding(DefaultParameterSetName = 'DirectVersion')]
 param (
-   # give a revision number if needed for test and build purposes
-   [Parameter(Mandatory = $false)]
+   # Revision number for non-production use
+   [Parameter(Mandatory = $true, ParameterSetName = 'Revision')]
    [int]
-   $RevisionNumber = -1,
-   # path to the module wihtout the name e.g. "C:\path\without\name" and not "C:\path\without\name\VSTeam"
+   $RevisionNumber,
+
+   # Path to the module without the name e.g. "C:\path\without\name" and not "C:\path\without\name\VSTeam"
    [Parameter(Mandatory = $true)]
    [string]
    $ModulePath
 )
-Write-Host "Compute Version Number"
 
 if ($null -eq (Get-Module -Name Metadata -ListAvailable)) {
-   Write-Host "Metadata module not installed, installing it"
    $null = Install-Module -Name Metadata -Scope CurrentUser -Force
 }
 
+Write-Host 'Compute Version Number'
+
 # Load the psd1 file so you can read the version
 $manifest = Import-Metadata -Path "$ModulePath/dist/VSTeam.psd1"
-# load as semantic version
-[version]$sem_version = $manifest.ModuleVersion
 
-# Build new semantic version with build number
-$package_version = "$($sem_version.Major).$($sem_version.Minor).$($sem_version.Build)"
-if ($RevisionNumber -and $RevisionNumber -ne -1) {
-   $package_version = "$($sem_version.Major).$($sem_version.Minor).$($sem_version.Build).$($RevisionNumber)"
-}
-$manifest.ModuleVersion = $package_version
-Write-Host "Package Version Number: $package_version"
+# Load as semantic version
+[version]$sem_version = $manifest.ModuleVersion
+# Build new semantic version with revision number
+$computedVersion = "$($sem_version.Major).$($sem_version.Minor).$($sem_version.Build).$($RevisionNumber)"
+
+$manifest.ModuleVersion = $computedVersion
+Write-Host "Package Version Number: $computedVersion"
 $null = Export-Metadata -Path "$ModulePath/dist/VSTeam.psd1" -InputObject $manifest
 
-Write-Output $package_version
+Write-Output $computedVersion
